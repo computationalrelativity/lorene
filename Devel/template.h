0981fccd5b07f70141283815860a14fdc4d18f2d
e_gourgoulhon <e_gourgoulhon>
2001-12-11 06:44:41 +0000

    template files

--- Devel/template.h
+++ Devel/template.h
@@ -9,9 +9,8 @@
  *   This file is part of LORENE.
  *
  *   LORENE is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
+ *   it under the terms of the GNU General Public License version 2
+ *   as published by the Free Software Foundation.
  *
  *   LORENE is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -30,8 +29,9 @@
 /*
  * $Id$
  * $Log$
- * Revision 1.1  2001/11/23 15:09:09  e_gourgoulhon
- * Templates for new source files
+ * Revision 1.2  2001/12/11 06:44:41  e_gourgoulhon
+ * template files
+ *
  *
  *
  * $Header$
