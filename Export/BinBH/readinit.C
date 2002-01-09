157de74bee071b4c460bf17a8a53daf06d7b5fc3
e_gourgoulhon <e_gourgoulhon>
2002-01-09 16:32:57 +0000

    Added the parameter fill in readinit.par Suppressed the multiple outputs in files ini* via method sauve Output directly written in readinit.C

--- Export/BinBH/readinit.C
+++ Export/BinBH/readinit.C
@@ -4,7 +4,7 @@
  */
 
 /*
- *   Copyright (c) 2001  Eric Gourgoulhon
+ *   Copyright (c) 2001-2002  Eric Gourgoulhon
  *
  *   This file is part of LORENE.
  *
@@ -28,6 +28,11 @@ char readinit_C[] = "$Header$" ;
 /*
  * $Id$
  * $Log$
+ * Revision 1.3  2002/01/09 16:32:57  e_gourgoulhon
+ * Added the parameter fill in readinit.par
+ * Suppressed the multiple outputs in files ini* via method sauve
+ * Output directly written in readinit.C
+ *
  * Revision 1.2  2001/12/18 09:08:14  e_gourgoulhon
  * Adds the filling of the holes interiors
  *
@@ -56,17 +61,18 @@ int main() {
     ifstream fparam("readinit.par") ; 
     char comment[120] ; 
     char datafile[120] ;
-    int nx, ny, nz ; 
-    double x_min, x_max, y_min, y_max, z_min, z_max ;  
+    int nx, ny, nz, fill ;
+    double x_min, x_max, y_min, y_max, z_min, z_max ;
+    fparam.getline(comment, 120) ;
+    fparam.getline(comment, 120) ;
+    fparam.getline(datafile, 120) ;
+    fparam.getline(comment, 120) ;
     fparam.getline(comment, 120) ; 
-    fparam.getline(comment, 120) ; 
-    fparam.getline(datafile, 120) ; 
-    fparam.getline(comment, 120) ; 
-    fparam.getline(comment, 120) ; 
-    fparam.getline(comment, 120) ; 
-    fparam >> nx ; fparam.getline(comment, 120) ; 
-    fparam >> ny ; fparam.getline(comment, 120) ; 
-    fparam >> nz ; fparam.getline(comment, 120) ; 
+    fparam.getline(comment, 120) ;
+    fparam >> fill ; fparam.getline(comment, 120) ;
+    fparam >> nx ; fparam.getline(comment, 120) ;
+    fparam >> ny ; fparam.getline(comment, 120) ;
+    fparam >> nz ; fparam.getline(comment, 120) ;
     fparam >> x_min ; fparam >> x_max ; fparam.getline(comment, 120) ; 
     fparam >> y_min ; fparam >> y_max ; fparam.getline(comment, 120) ; 
     fparam >> z_min ; fparam >> z_max ; fparam.getline(comment, 120) ; 
@@ -95,7 +101,7 @@ int main() {
     double dx = (x_max - x_min) / double(nx - 1) ;     
     double dy = (y_max - y_min) / double(ny - 1) ;     
     double dz = (z_max - z_min) / double(nz - 1) ;   
-    
+
     double* pxi = xi ;
     double* pyi = yi ;
     double* pzi = zi ;
@@ -120,55 +126,73 @@ int main() {
 	    }
 	}
     }
-    
+
     // Read of the initial data file and computation of the
     //  fields at the Cartesian grid points
     // ----------------------------------------------------
 
-    int fill = 1 ;  // type of filling
     Bin_BH binary(nbp, xi, yi, zi, fill, datafile) ;
 
-    cout << binary << endl ; 
-    
+    cout << binary << endl ;
+
+    ofstream fich("readout.d") ;
+
+    fich.precision(13) ;
+    fich.setf(ios::scientific, ios::floatfield) ;
+
+    fich << binary.dist << " dist [a]" << endl ;
+    fich << "nx, ny, nz : " << nx << "  " << ny << "  " << nz << endl ;
+    fich << "x_min, dx : " << x_min << "  " << dx << endl ;
+    fich << "y_min, dy : " << y_min << "  " << dy << endl ;
+    fich << "z_min, dz : " << z_min << "  " << dz << endl ;
+
+    int index = 0 ;
+
+    for (int k=0; k<nz; k++) {
+
+	for (int j=0; j<ny; j++) {
+
+	    for (int i=0; i<nx; i++) {
+
+                fich << i
+                  << " " <<  binary.g_xx[index] << " " <<  binary.g_xy[index] << " " <<  binary.g_xz[index]
+                  << " " <<  binary.g_yy[index] << " " <<  binary.g_yz[index] << " " <<  binary.g_zz[index]
+                  << " " <<  binary.k_xx[index] << " " <<  binary.k_xy[index] << " " <<  binary.k_xz[index]
+                  << " " <<  binary.k_yy[index] << " " <<  binary.k_yz[index] << " " <<  binary.k_zz[index]
+                  << " " <<  binary.beta_x[index] << " " <<  binary.beta_y[index]
+                  << " " <<  binary.beta_z[index]  << " " <<  binary.nnn[index] << endl ;
+
+                index++ ;
+
+                        }
+                }
+
+        }
+
+
+    fich.close() ;
+
+
+
     // Save in a binary file
     // ---------------------
-    FILE* finib = fopen("inib.d", "w") ; 
-    binary.save_bin(finib) ; 
-    fclose( finib ) ; 
-    
+    // FILE* finib = fopen("inib.d", "w") ;
+    // binary.save_bin(finib) ;
+    // fclose( finib ) ;
+
     // Save in a formatted file
     // ------------------------
-    ofstream finif("inif.d") ; 
-    binary.save_form(finif) ; 
-    finif.close() ; 
-    
-    // Create a new object from a binary file
-    //---------------------------------------
-    finib = fopen("inib.d", "r") ; 
-    Bin_BH binary2(finib) ; 
-    fclose( finib ) ; 
+    // ofstream finif("inif.d") ;
+    // binary.save_form(finif) ;
+    // finif.close() ;
 
-    finib = fopen("inib2.d", "w") ; 
-    binary2.save_bin(finib) ; 
-    fclose( finib ) ; 
-    
-    // Create a new object from a formatted file
-    //---------------------------------------
-    ifstream finif3("inif.d") ; 
-    Bin_BH binary3(finif3) ; 
-    finif3.close() ; 
-
-    finif.open("inif3.d") ; 
-    binary3.save_form(finif) ; 
-    finif.close() ; 
-    
 
     // Clean exit
     // ----------
-    
-    delete [] xi ; 
-    delete [] yi ; 
-    delete [] zi ; 
-   
-    return EXIT_SUCCESS ; 
+
+    delete [] xi ;
+    delete [] yi ;
+    delete [] zi ;
+
+    return EXIT_SUCCESS ;
 }
