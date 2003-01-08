6e2a2768110a285682548202ed757b74c4996e40
e_gourgoulhon <e_gourgoulhon>
2003-01-08 23:59:19 +0000

    Added the instructions: using std::setw ; etc...

--- C++/Include/headcpp.h
+++ C++/Include/headcpp.h
@@ -1,10 +1,27 @@
 #ifdef OBSOLETE_HEADERS
+
 #include <iomanip.h>
 #include <iostream.h>
 #include <fstream.h>
+
 #else
+
 #include <iomanip>
+using std::setw ;
+using std::hex ;
 #include <iostream>
+using std::ostream ;
+using std::istream ;
+using std::cout ;
+using std::cin ;
+using std::cerr ;
+using std::clog ;
+using std::endl ;
+using std::flush ;
+using std::ios ;
 #include <fstream>
+using std::ifstream ;
+using std::ofstream ;
+
 #endif
 
