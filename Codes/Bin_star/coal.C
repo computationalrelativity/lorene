fabb3bd0a87d37d947d4c8d67a5b87a0d9f1100f
e_gourgoulhon <e_gourgoulhon>
2003-02-20 15:08:42 +0000

    Suppressed the qualifier ios::nocreate in call to fstream::open (not supported by gcc 3.2).

--- Codes/Bin_star/coal.C
+++ Codes/Bin_star/coal.C
@@ -30,6 +30,10 @@ char coal_C[] = "$Header$" ;
 /*
  * $Id$
  * $Log$
+ * Revision 1.5  2003/02/20 15:08:42  e_gourgoulhon
+ * Suppressed the qualifier ios::nocreate in call to fstream::open
+ * (not supported by gcc 3.2).
+ *
  * Revision 1.4  2003/01/09 11:07:48  j_novak
  * headcpp.h is now compliant with C++ norm.
  * The include files have been ordered, as well as the local_settings_linux
@@ -993,7 +997,7 @@ int main(){
     system("cat parcoal.d >> calcul.d") ; 
 
     // Identification du code et de ses sous-routines (no. de version RCS) :     	
-    fichfinal.open("calcul.d", ios::app | ios::nocreate) ; 
+    fichfinal.open("calcul.d", ios::app ) ; 
     fichfinal << endl <<
     "================================================================" << endl ; 
     fichfinal << "	    IDENTIFICATION OF THE CODE : " << endl ; 
