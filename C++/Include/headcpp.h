a7fce582391bb3b79793fb3ec573956c970ca094
j_novak <j_novak>
2003-10-03 07:24:11 +0000

    Added and include of typeinfo

--- C++/Include/headcpp.h
+++ C++/Include/headcpp.h
@@ -3,12 +3,14 @@
 #include <iomanip.h>
 #include <iostream.h>
 #include <fstream.h>
+#include <typeinfo.h>
 
 #else
 
 #include <iomanip>
 #include <iostream>
 #include <fstream>
+#include <typeinfo>
 using namespace std ;
 #endif
 
