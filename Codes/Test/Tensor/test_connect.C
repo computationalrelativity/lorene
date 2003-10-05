99bf9e26ac8c22c1cd1e1dd03afa016c31ea2350
e_gourgoulhon <e_gourgoulhon>
2003-10-05 21:18:08 +0000

    Added test onto a vector field.

--- Codes/Test/Tensor/test_connect.C
+++ Codes/Test/Tensor/test_connect.C
@@ -28,6 +28,9 @@ char test_connect_C[] = "$Header$" ;
 /*
  * $Id$
  * $Log$
+ * Revision 1.3  2003/10/05 21:18:08  e_gourgoulhon
+ * Added test onto a vector field.
+ *
  * Revision 1.2  2003/10/03 14:27:51  e_gourgoulhon
  * First non trivial test (successfull !).
  *
@@ -54,13 +57,15 @@ char test_connect_C[] = "$Header$" ;
 
 int main() {
 
+	int arret = 1 ; 
+
 	// Construction of a multi-grid (Mg3d)
 	// -----------------------------------
   
 	int nz = 3 ; 	// Number of domains
 	int nr = 5 ; 	// Number of collocation points in r in each domain
 	int nt = 5 ; 	// Number of collocation points in theta in each domain
-	int np = 4 ; 	// Number of collocation points in phi in each domain
+	int np = 8 ; 	// Number of collocation points in phi in each domain
 	int symmetry_theta = SYM ; // symmetry with respect to the equatorial plane
 	int symmetry_phi = NONSYM ; // no symmetry in phi
 	bool compact = true ; // external domain is compactified
@@ -88,35 +93,47 @@ int main() {
 	Connection_fcart derc(map, map.get_bvect_cart()) ; 
 
 
+	cout << endl << 
+	"================ TEST FOR A SCALAR FIELD =================\n" ; 
+	
+
 	// Construction of a scalar field (Scalar)
 	// ---------------------------------------
 
 	const Coord& x = map.x ; 
+	const Coord& z = map.z ; 
+	const Coord& r = map.r ; 
+	const Coord& sint = map.sint ; 
+	const Coord& sinp = map.sinp ; 
 
 	Scalar uu(map) ; 
+	Scalar tmp(map) ; 
 
 	uu = x ; 	
-	uu.annule(nz-1,nz-1) ; 	// zero in the last domain
+	tmp = sint * sinp / r ;
+	uu.set_domain(nz-1) = tmp.domain(nz-1) ; // y/r^2 in the external domain
 	
-	uu.std_spectral_base_scal() ;   // sets the standard spectral basis for 
+	uu.std_spectral_base() ;   // sets the standard spectral basis for 
 									// expansion of a scalar field
 									
 	cout << "uu : " << uu << endl ; 
 	uu.spectral_display(cout) ; 
-	arrete() ; 
+	arrete(arret) ; 
 	
 	// Gradient of the scalar field
 	// ----------------------------
 	
 	Vector duuc = derc.derive_cov(uu)  ;  
+
 	cout << "duuc : " << duuc << endl ; 
 
-	arrete() ; 
+	arrete(arret) ; 
 
 	Vector duus = ders.derive_cov(uu) ; 
+	
 	cout << "duus : " << duus << endl ; 
 
-	arrete() ; 
+	arrete(arret) ; 
 	
 	// Test
 	// ----
@@ -131,7 +148,7 @@ int main() {
 	for (int i=1; i<=3; i++) {
 		cout << norme(diffc(i)) << endl ; 
 	}
-	arrete() ; 
+	arrete(arret) ; 
 	
 	
 	Vector duuc_s = duuc ; 
@@ -144,7 +161,64 @@ int main() {
 	for (int i=1; i<=3; i++) {
 		cout << norme(diffs(i)) << endl ; 
 	}
+	arrete(arret) ; 
 	
+	cout << endl << 
+	"================ TEST FOR A VECTOR FIELD =================\n" ; 
+	
+	Vector vvc(map, COV, map.get_bvect_cart()) ; 
+	
+	vvc.set(1) = uu ; 
+	vvc.set(2) = uu*uu ; 
+	vvc.set(3) = 0 ; 
+	vvc.set(3) = z + x * z ; 
+	vvc.set(3).set_domain(nz-1) = duuc(3).domain(nz-1) ; 
+	
+	vvc.std_spectral_base() ; 
+	Tensor dvvc = derc.derive_cov(vvc) ;
+	
+	// cout << "dvvc : " << dvvc << endl ; 
+	
+	Vector vvs = vvc ; 
+	vvs.change_triad( map.get_bvect_spher() ) ; 
+	
+	Tensor dvvs = ders.derive_cov(vvs) ; 
+	
+	Tensor dvvs_c = dvvs ; 
+	dvvs_c.change_triad( map.get_bvect_cart() ) ; 
+	
+	Tensor diffvvc = dvvs_c - dvvc ; 
+	
+	cout << "dvvc(1,1) : "  << endl ;
+	dvvc(1,1).spectral_display(cout) ;  
+	arrete() ; 
+	
+	cout << "dvvs_c(1,1) : " << endl ; 
+	dvvs_c(1,1).spectral_display(cout) ;  
+	arrete() ; 
+	
+	cout << "Norm of diffvvc: " << endl ; 
+	for (int i=1; i<=3; i++) {
+		for (int j=1; j<=3; j++) {
+			cout << i << " " << j << " : " << norme(diffvvc(i,j)) << endl ; 
+		}
+	}
+	arrete(arret) ; 
 
+
+
+	Tensor dvvc_s = dvvc ; 
+	dvvc_s.change_triad( map.get_bvect_spher() ) ; 
+
+	Tensor diffvvs = dvvc_s - dvvs ; 
+	
+	cout << "Norm of diffvvs: " << endl ; 
+	for (int i=1; i<=3; i++) {
+		for (int j=1; j<=3; j++) {
+			cout << i << " " << j << " : " << norme(diffvvs(i,j)) << endl ; 
+		}
+	}
+	arrete(arret) ; 
+	
 	return EXIT_SUCCESS ; 
 }
