615eca9380b383c0164c2819d8624921d13a0d97
k_taniguchi <k_taniguchi>
2003-11-28 04:30:22 +0000

    Add the output operator.

--- C++/Source/Etoile/et_bin_nsbh.C
+++ C++/Source/Etoile/et_bin_nsbh.C
@@ -30,6 +30,9 @@ char et_bin_nsbh_C[] = "$Header$" ;
 /*
  * $Id$
  * $Log$
+ * Revision 1.4  2003/11/28 04:30:22  k_taniguchi
+ * Add the output operator.
+ *
  * Revision 1.3  2003/10/24 12:30:58  k_taniguchi
  * Add some derivative terms
  *
@@ -273,3 +276,129 @@ void Et_bin_nsbh::sauve(FILE* fich) const {
 
 // Printing
 // --------
+
+ostream& Et_bin_nsbh::operator>>(ostream& ost) const {
+
+    #include "unites.h"
+    // To avoid some compilation warnings
+    if (&ost == 0x0) {
+      cout << qpig << msol << f_unit << mevpfm3 << endl ;
+    }
+
+    Etoile::operator>>(ost) ;
+
+    ost << endl ;
+    ost << "Neutron star in a binary system" << endl ;
+    ost << "-------------------------------" << endl ;
+
+    if (irrotational) {
+      ost << "irrotational configuration" << endl ;
+    }
+    else {
+      ost << "corotating configuration" << endl ;
+    }
+
+    ost << "Absolute abscidia of the stellar center: " <<
+      mp.get_ori_x() / km << " km" << endl ;
+
+    ost << "Absolute abscidia of the barycenter of the baryon density : " <<
+      xa_barycenter() / km << " km" << endl ;
+
+    double r_0 = 0.5 * ( ray_eq() + ray_eq_pi() ) ;
+    double d_ns = fabs( mp.get_ori_x() ) + ray_eq_pi() - r_0 ;
+    double d_tilde = 2 * d_ns / r_0 ;
+
+    ost << "d_tilde : " << d_tilde << endl ;
+
+    ost << "Orientation with respect to the absolute frame : " <<
+      mp.get_rot_phi() << " rad" << endl ;
+
+    ost << "Central value of gam_euler : "
+	<< gam_euler()(0, 0, 0, 0)  << endl ;
+
+    ost << "Central u_euler (U^X, U^Y, U^Z) [c] : "
+	<< u_euler(0)(0, 0, 0, 0) << "  "
+	<< u_euler(1)(0, 0, 0, 0) << "  "
+	<< u_euler(2)(0, 0, 0, 0) << endl ;
+
+    if (irrotational) {
+      ost << "Central d_psi (X, Y, Z) [c] :         "
+	  << d_psi(0)(0, 0, 0, 0) << "  "
+	  << d_psi(1)(0, 0, 0, 0) << "  "
+	  << d_psi(2)(0, 0, 0, 0) << endl ;
+
+      ost << "Central vel. / co-orb. (W^X, W^Y, W^Z) [c] : "
+	  << wit_w(0)(0, 0, 0, 0) << "  "
+	  << wit_w(1)(0, 0, 0, 0) << "  "
+	  << wit_w(2)(0, 0, 0, 0) << endl ;
+
+      ost << "Max vel. / co-orb. (W^X, W^Y, W^Z) [c] : "
+	  << max(max(wit_w(0))) << "  "
+	  << max(max(wit_w(1))) << "  "
+	  << max(max(wit_w(2))) << endl ;
+
+      ost << "Min vel. / co-orb. (W^X, W^Y, W^Z) [c] : "
+	  << min(min(wit_w(0))) << "  "
+	  << min(min(wit_w(1))) << "  "
+	  << min(min(wit_w(2))) << endl ;
+
+      double r_surf = mp.val_r(0,1.,M_PI/4,M_PI/4) ;
+
+      ost << "Velocity at (r_surf,pi/4,pi/4) / co-orb. [c] : "
+	  << wit_w(0).val_point(r_surf,M_PI/4,M_PI/4) << "  "
+	  << wit_w(1).val_point(r_surf,M_PI/4,M_PI/4) << "  "
+	  << wit_w(2).val_point(r_surf,M_PI/4,M_PI/4) << endl ;
+
+      ost << "Central value of loggam : "
+	  << loggam()(0, 0, 0, 0)  << endl ;
+    }
+
+    ost << "Central value of lapse(N) auto :             "
+	<< n_auto()(0, 0, 0, 0) << endl ;
+
+    ost << "Central value of confpsi auto :              "
+	<< confpsi_auto()(0, 0, 0, 0) << endl ;
+
+    ost << "Central value of shift (N^X, N^Y, N^Z) [c] : "
+	<< shift(0)(0, 0, 0, 0) << "  "
+	<< shift(1)(0, 0, 0, 0) << "  "
+	<< shift(2)(0, 0, 0, 0) << endl ;
+
+    ost << "  ... shift_auto part of it [c] :            "
+	<< shift_auto(0)(0, 0, 0, 0) << "  "
+	<< shift_auto(1)(0, 0, 0, 0) << "  "
+	<< shift_auto(2)(0, 0, 0, 0) << endl ;
+
+    ost << "  ... shift_comp part of it [c] :            "
+	<< shift_comp(0)(0, 0, 0, 0) << "  "
+	<< shift_comp(1)(0, 0, 0, 0) << "  "
+	<< shift_comp(2)(0, 0, 0, 0) << endl ;
+
+    ost << "  ... w_shift (NB: components in the star Cartesian frame) [c] :  "
+	<< endl
+	<< w_shift(0)(0, 0, 0, 0) << "  "
+	<< w_shift(1)(0, 0, 0, 0) << "  "
+	<< w_shift(2)(0, 0, 0, 0) << endl ;
+
+    ost << "Central value of khi_shift [km c] : "
+        << khi_shift()(0, 0, 0, 0) / km << endl ;
+
+    ost << endl << "Central value of (B^X, B^Y, B^Z)/N [c] : "
+	<< bsn(0)(0, 0, 0, 0) << "  "
+	<< bsn(1)(0, 0, 0, 0) << "  "
+	<< bsn(2)(0, 0, 0, 0) << endl ;
+
+    ost << endl <<
+      "Central (d/dX,d/dY,d/dZ)(logn_auto) [km^{-1}] : "
+	<< d_n_auto(0)(0, 0, 0, 0) * km << "  "
+	<< d_n_auto(1)(0, 0, 0, 0) * km << "  "
+	<< d_n_auto(2)(0, 0, 0, 0) * km << endl ;
+
+    ost << endl <<
+      "Central (d/dX,d/dY,d/dZ)(beta_auto) [km^{-1}] : "
+	<< d_confpsi_auto(0)(0, 0, 0, 0) * km << "  "
+	<< d_confpsi_auto(1)(0, 0, 0, 0) * km << "  "
+	<< d_confpsi_auto(2)(0, 0, 0, 0) * km << endl ;
+
+    return ost ;
+}
