18da73c2253634839428fddd05ff2b753c0a17e6
k_taniguchi <k_taniguchi>
2003-11-29 07:01:25 +0000

    Large modification. However, the code has not worked yet.

--- Codes/Bin_ns_bh/coal_ns_bh.C
+++ Codes/Bin_ns_bh/coal_ns_bh.C
@@ -29,6 +29,9 @@ char coal_ns_bh_C[] = "$Header$" ;
 /*
  * $Id$
  * $Log$
+ * Revision 1.3  2003/11/29 07:01:25  k_taniguchi
+ * Large modification. However, the code has not worked yet.
+ *
  * Revision 1.2  2002/12/19 14:58:20  e_gourgoulhon
  * First non-empty version.
  *
@@ -55,7 +58,7 @@ char coal_ns_bh_C[] = "$Header$" ;
 #include "eos.h"
 #include "utilitaires.h"
 
-int  main(){
+int main(){
 
 
     // Identification of all the subroutines called by the code :
@@ -141,7 +144,7 @@ int  main(){
 	 << mermax << endl ;
     cout << "Relaxation factor in the main iteration  : "
 	 << relax << endl ;
-    cout << "Maximum number of steps in Etoile_bin::equilibrium : "
+    cout << "Maximum number of steps in Et_bin_nsbh::equilibrium_nsbh : "
 	 << mermax_eqb << endl ;
     cout << "Threshold on the enthalpy relative change for ending the computation : "
 	 << seuil << endl ;
@@ -196,22 +199,25 @@ int  main(){
     Mg3d mg_bh(fich) ;
     Map_af mp_bh(mg_bh, fich) ;
 
+    cout << "Make an object of Bin_ns_bh" << endl ;
+    arrete(prompt) ;
     Bin_ns_bh bibi(mp_ns, *peos, mp_bh, fich) ;
 
     fclose(fich) ;
-
 
     //------------------------------------------------------------------
     //	    Modification of the separation between the two stars
     //------------------------------------------------------------------
 
-        double ori_x = ((bibi.get_ns()).get_mp()).get_ori_x() ;
-        ori_x *= fact_separ ;
-        ((bibi.set_ns()).set_mp()).set_ori(ori_x, 0., 0.) ;
+    cout << "Modification of the orbital separation" << endl ;
+    arrete(prompt) ;
+    double ori_x = ((bibi.get_ns()).get_mp()).get_ori_x() ;
+    ori_x *= fact_separ ;
+    ((bibi.set_ns()).set_mp()).set_ori(ori_x, 0., 0.) ;
 
-        ori_x = ((bibi.get_bh()).get_mp()).get_ori_x() ;
-        ori_x *= fact_separ ;
-        ((bibi.set_bh()).set_mp()).set_ori(ori_x, 0., 0.) ;
+    ori_x = ((bibi.get_bh()).get_mp()).get_ori_x() ;
+    ori_x *= fact_separ ;
+    ((bibi.set_bh()).set_mp()).set_ori(ori_x, 0., 0.) ;
 
 
     //------------------------------------------------------------------
@@ -220,14 +226,477 @@ int  main(){
 
     // Initialisation of A^2, N, etc...
     // ---------------------------------
+    cout << "Update the initial conditions for NS" << endl ;
+    arrete(prompt) ;
     (bibi.set_ns()).update_metric( bibi.get_bh() ) ;
+    // ????? arguments ?????
+    cout << "Update the initial conditions for BH" << endl ;
+    arrete(prompt) ;
+    (bibi.set_bh()).update_metric( bibi.get_ns(), 1., 1.) ; // double, double
 
     // Initialisation of gradients of companion potentials
     // ---------------------------------------------------
-
     (bibi.set_ns()).update_metric_der_comp( bibi.get_bh() ) ;
 
-        bibi.set_ns().equation_of_state() ;
-	cout << "Initial data: " << bibi << endl ; 
+    (bibi.set_bh()).fait_n_comp( bibi.get_ns() ) ;
+    (bibi.set_bh()).fait_psi_comp( bibi.get_ns() ) ;
+
+    bibi.fait_decouple() ;
+    bibi.fait_tkij() ;
+
+    // Initialisation of hydro quantities for NS
+    // -----------------------------------------
+    (bibi.set_ns()).equation_of_state() ;
+    (bibi.set_ns()).kinematics(bibi.get_omega(), bibi.get_x_axe()) ;
+    (bibi.set_ns()).fait_d_psi() ;
+
+    // Save in a file
+    // --------------
+    FILE* fresu = fopen("resu.d", "w") ;
+
+    int mer1 = 0 ;
+    fwrite(&mer1, sizeof(int), 1, fresu) ;	// mer
+
+    (bibi.get_ns()).get_mp().get_mg()->sauve(fresu) ;
+    (bibi.get_ns()).get_mp().sauve(fresu) ;
+    (bibi.get_ns()).get_eos().sauve(fresu) ;
+
+    (bibi.get_bh()).get_mp().get_mg()->sauve(fresu) ;
+    (bibi.get_bh()).get_mp().sauve(fresu) ;
+
+    bibi.sauve(fresu) ;
+
+    fclose(fresu) ;
+
+    cout << endl
+	 << "======================================================" << endl
+	 << "                  Initial conditions                  " << endl
+	 << "======================================================" << endl ;
+    cout << bibi << endl ;
+
+    if ( (bibi.get_ns()).is_relativistic() ) {
+      cout << "========================" << endl ;
+      cout << "Relativistic computation" << endl ;
+      cout << "========================" << endl ;
+    }
+    else {
+      cout << "===========================================" << endl ;
+      cout << "!!! WARNING : NS should be relativistic !!!" << endl ;
+      cout << "===========================================" << endl ;
+      abort() ;
+    }
+
+    arrete(prompt) ;
+
+
+    //----------------------------------------------------------------
+    //			Auxiliary quantities
+    //----------------------------------------------------------------
+
+    double ent_c ;     // Central enthaply in NS
+    double dentdx ;    // Central d/dx(enthalpy) in NS
+
+    // Resizing factor of the first shell
+    // Computation is shown just befor "equilibrium" of NS
+    Tbl fact_resize[] = {Tbl(1)} ;
+    fact_resize[0].set_etat_qcq() ;
+
+    // Error indicators in NS
+    Tbl differ[] = {Tbl(7)} ;
+    differ[0].set_etat_qcq() ;
+
+    ent_c = (bibi.get_ns()).get_ent()()(0, 0, 0, 0) ;
+    differ[0].set(0) = 1 ;	    // diff_ent = 1
+    differ[0].set(1) = 1 ;	    // err_psi = 1
+    differ[0].set(2) = 1 ;	    // err_n = 1
+    differ[0].set(3) = 1 ;	    // err_confpsi = 1
+    differ[0].set(4) = 1 ;	    // err_shift_x = 1
+    differ[0].set(5) = 1 ;	    // err_shift_y = 1
+    differ[0].set(6) = 1 ;	    // err_shift_z = 1
+
+    double relax_jm1 = 1. - relax ;
+    double relax_omeg_jm1 = 1. - relax_omeg ;
+
+    //----------------------------------------------------------------
+    //	 Binary system at the previous step (for the relaxation)
+    //----------------------------------------------------------------
+
+    Bin_ns_bh bibi_jm1 = bibi ;
+
+    double omega_jm1 = bibi_jm1.get_omega() ;
+
+    // n_comp and pot_centri of NS are initialized to 0.5 and 0 on bibi_jm1
+    // --------------------------------------------------------------------
+    bibi_jm1.set_ns().set_n_comp() = 0.5 ;
+    bibi_jm1.set_ns().set_pot_centri() = 0 ;
+
+    //---------------------------
+    //	 Openning of log files
+    //---------------------------
+
+    ofstream fichresu("resglob.d") ;
+    fichresu.precision(16) ;
+
+    ofstream fichrota("resrota.d") ;
+    fichrota.precision(16) ;
+
+    ofstream fichconv("resconv.d") ;
+    fichconv.precision(16) ;
+
+    ofstream fichet("resstar.d") ;
+    fichet.precision(16) ;
+
+    fichrota <<
+      "#      Omega [rad/s]        x_axe [km]        x_g (NS) [km]        x_g (BH) [km]        M_grav [M_sol]        J [G M_sol^2/c]"
+	     << endl ;
+
+    fichconv <<
+      "#      diff_ent        err_psi        err_n        err_confpsi        err_shift_x        err_shift_y        err_shift_z"
+	     << endl ;
+
+    fichet <<
+      "#      ori_x [km]        ent_c        M_bar [M_sol]        R(theta=0) [km]        R(pi/2, 0) [km]        R(pi/2, pi/2) [km]        R(pi/2, pi) [km]"
+	   << endl ;
+
+    //    double omega_kep ;
+    int mer ;
+
+//=========================================================================
+//		Start of iteration
+//=========================================================================
+
+    for (mer=0; (differ[0](0) > seuil) && (mer < mermax); mer++) {
+
+      cout <<
+	"================================================================"
+	   << endl ;
+      cout << "step = " << mer << "        diff_ent : "
+	   << differ[0](0) << endl ;
+      cout <<
+	"================================================================"
+	   << endl ;
+
+      fichresu << mer;
+      fichresu << "    step" << endl ;
+
+      fichrota << mer ;
+      fichconv << mer ;
+      fichet << mer ;
+
+      //------------------------------------------------
+      //     Computation of the metric coefficients
+      //------------------------------------------------
+
+      if ( (mer % fmer_upd_met) == 0 ) {
+
+	(bibi.set_ns()).update_metric(bibi.get_bh()) ;
+
+	(bibi.set_ns()).update_metric_der_comp(bibi.get_bh()) ;
+
+      }
+
+      //------------------------------------------------------------
+      //     Computation of the orbital angular velocity Omega
+      //------------------------------------------------------------
+
+      bibi.orbit_omega(fact_omeg_min, fact_omeg_max) ;
+
+      // Translation of the stars in order to set the origin
+      //  of the absolute frame on the rotation axis
+      //-----------------------------------------------------
+      // !!! The subroutine for computation of the position of ratation axis
+      // !!!  has not been developped
+
+      cout << display_bold << "New orbital velocity Omega : "
+	   << bibi.get_omega() * f_unit << " rad/s" << display_normal << endl ;
+
+      // Keplerian velocity (for comparison only)
+      // ----------------------------------------
+      /*
+      omega_kep = 0. ;   // I do not know whether it is necessary or not.
+
+      cout << "``Keplerian'' velocity (for comparison only) : "
+	   << omega_kep * f_unit << " rad/s" << endl ;
+      */
+      /*
+      cout << "New X coordinate of the rotation axis : "
+	   << bibi.get_x_axe() / km << " km" << endl ;
+      */
+
+      arrete(prompt) ;
+
+      fichresu << bibi.get_x_axe() / km ;
+      fichresu << "    abscidia of the rotation axis [km] " << endl ;
+
+      fichresu << bibi.get_omega() * f_unit ;
+      fichresu << "    Orbital frequency Omega [rad/s] " << endl ;
+
+      fichrota << "  " << bibi.get_omega() * f_unit ;
+      fichrota << "  " << bibi.get_x_axe() / km ;
+
+      //---------------------------------------------------------
+      //     Computation of B^i/N (bsn) and pot_centri in NS
+      //---------------------------------------------------------
+
+      (bibi.set_ns()).kinematics( bibi.get_omega(), bibi.get_x_axe() ) ;
+
+      //------------------------------------------------------------------
+      //     Computation of gam_euler, u_euler, ener_euler, s_euler,
+      //     wit_w and loggam in NS
+      //------------------------------------------------------------------
+
+      (bibi.set_ns()).fait_d_psi() ;
+      (bibi.set_ns()).hydro_euler() ;
+
+      // Check of the Bin_ns_bh::orbit_omega computation
+      //------------------------------------------------
+      // !!! These out-put has not been prepared.
+
+      //---------------------------------------------------------------
+      //     Computation of the stellar equilibrium configurations
+      //---------------------------------------------------------------
+
+      /*
+      // !!! This rotine has not worked yet.
+      // Computation of the resizing factor
+      double ray_eq_auto = (bibi.get_ns()).ray_eq() ;
+      double ray_eq_comp = (bibi.get_bh()).ray_eq() ;
+
+      int num_resize ;
+
+      if (mg_ns.get_nzone() > 3) {
+	num_resize = mg_ns.get_nzone() - 3 ;
+      }
+      else {
+	num_resize = (bibi.get_ns()).get_nzet() ;
+      }
+
+      double lambda_resize = 0.95 *
+	(bibi.separation() - ray_eq_comp)/ray_eq_auto ;
+      fact_resize[0].set(0) =
+	(lambda_resize < 2.*num_resize) ? lambda_resize : 2.*num_resize ;
+      */
+
+      // Relaxation on n_comp (only if it has not been done by update_metric)
+      // --------------------------------------------------------------------
+      if ( (ind_rel_met == 0) || ( (mer % fmer_upd_met) != 0 ) ) {
+	    bibi.set_ns().set_n_comp() = relax * (bibi.get_ns()).get_n_comp()
+	      + relax_jm1 * (bibi_jm1.get_ns()).get_n_comp() ;
+      }
+
+      // Relaxation on pot_centri
+      // ------------------------
+      bibi.set_ns().set_pot_centri() = relax * (bibi.get_ns()).get_pot_centri()
+	+ relax_jm1 * (bibi_jm1.get_ns()).get_pot_centri() ;
+
+      // Call to Et_bin_nsbh::equilibrium_nsbh
+      // -------------------------------------
+
+      (bibi.set_ns()).equilibrium_nsbh(ent_c, mermax_eqb,
+				       mermax_poisson, relax_poisson,
+				       mermax_potvit, relax_potvit,
+				       thres_adapt, fact_resize[0],
+				       differ[0]) ;
+
+    //------------------------------------------------------------------
+    //	  Relaxations
+    //------------------------------------------------------------------
+
+    bibi.set_ns().relaxation( bibi_jm1.get_ns(), relax, relax_met, mer,
+			      fmer_upd_met ) ;
+
+    bibi.set_ns().hydro_euler() ;
+
+    //------------------------------------------------------------------
+    //	  Change in the central enthalpy to get a fixed baryon mass
+    //------------------------------------------------------------------
+
+    if (mer >= mer_masse) {
+
+      double xx = (bibi.get_ns()).mass_b() / mbar_voulue - 1. ;
+
+      cout << "Discrepancy M_b / wanted M_b : " << xx << endl ;
+
+      double xprog = ( mer > 2*mer_masse) ? 1. :
+	double(mer-mer_masse)/double(mer_masse) ;
+
+      xx *= xprog ;
+      double ax = .5 * ( 2. + xx ) / (1. + xx ) ;
+
+      double fact_ent = pow(ax, aexp_masse) ;
+
+      cout << "  xprog, xx, ax, fact : " << xprog << "  " << xx
+	   << "  " << ax << "  " << fact_ent << endl ;
+
+      ent_c *= fact_ent ;
+
+    }
+
+    // Updates for the next step
+    // -------------------------
+
+    bibi_jm1 = bibi ;
+
+    cout << bibi << endl ;
+
+    // Graphical output
+    // ----------------
+    // !!! This routine has not worked yet.
+
+    //-----------------------------------------------------------------------
+    //     The whole configuration is saved in a file
+    //-----------------------------------------------------------------------
+
+    if ( (mer % fmer_save) == 0 ) {
+
+      FILE* fresud = fopen("resu.d", "w") ;
+
+      fwrite(&mer, sizeof(int), 1, fresud) ;	// mer
+
+      (bibi.get_ns()).get_mp().get_mg()->sauve(fresud) ;
+      (bibi.get_ns()).get_mp().sauve(fresud) ;
+      (bibi.get_ns()).get_eos().sauve(fresud) ;
+
+      (bibi.get_bh()).get_mp().get_mg()->sauve(fresud) ;
+      (bibi.get_bh()).get_mp().sauve(fresud) ;
+
+      bibi.sauve(fresud) ;
+
+      fclose(fresud) ;
+
+    }
+
+    //--------------------------------------------
+    //  Writing of global quantities in log files
+    //--------------------------------------------
+    // !!! This routine has not worked yet.
+
+    }	// End of the main loop (mer)
+
+//=========================================================================
+//		End of iteration
+//=========================================================================
+
+    fichresu.close() ;
+    fichrota.close() ;
+    fichconv.close() ;
+    fichet.close() ;
+
+    //-------------------------------------------------------------
+    // General features of the final configuration saved in a file
+    //-------------------------------------------------------------
+
+    ofstream fichfinal("calcul.d") ;
+    fichfinal.precision(6) ;
+
+    time_t rawtime = time(0x0) ;
+    fichfinal << "Date: " << asctime( localtime( &rawtime ) ) << endl ;
+    char* hostname = getenv("HOST") ;
+    if (hostname != 0x0) {
+      fichfinal << "Computer: " << hostname << endl ;
+    }
+    fichfinal <<
+      "==================================================================="
+	      << endl << endl ;
+
+    if ( (bibi.get_ns()).is_irrotational() ) {
+      fichfinal << "                           Irrotational" << endl ;
+    }
+    else {
+      fichfinal << "                           Co-rotating" << endl ;
+    }
+
+    fichfinal << (bibi.get_ns()).get_eos() << endl ;
+
+    fichfinal << "Omega = " << bibi.get_omega() * f_unit << " rad/s"
+	      << "                Orbital frequency f = "
+	      << bibi.get_omega() / (2*M_PI) * f_unit << " Hz" << endl ;
+    //    fichfinal << "Omega_kepler = " << omega_kep * f_unit << " rad/s" << endl ;
+    fichfinal << "Coordinate separation : " << bibi.separation()  / km
+	      << " km" << endl ;
+    // ADM mass has not been defined.
+    /*
+    fichfinal << "1/2 ADM mass :        " << 0.5 * bibi.mass_adm() / msol
+	      << " Mo" << endl ;
+    */
+    // Angular momentum has not been defined.
+    /*
+    fichfinal << "Total angular momentum : "
+	      << bibi.angu_mom()(2)/ ( qpig / (4* M_PI) * msol*msol)
+	      << " G M_sol^2 / c" << endl ;
+    */
+
+    fichfinal << endl << "Number of steps : " << mer << endl ;
+
+    fichfinal << endl <<
+      "==================================================================="
+	      << endl ;
+    fichfinal << "       Neutron Star" << endl ;
+    fichfinal <<
+      "==================================================================="
+	      << endl ;
+    fichfinal << "Grid : " << endl ;
+    fichfinal << "------ " << endl ;
+    fichfinal << *((bibi.get_ns()).get_mp().get_mg()) << endl ;
+    fichfinal << endl << "Physical characteristics : " << endl ;
+    fichfinal	  << "-------------------------" << endl ;
+    fichfinal << bibi.get_ns() << endl ;
+
+    // !!! It is necessary to save the information of EOS here.
+
+    fichfinal << endl <<
+      "==================================================================="
+	      << endl ;
+    fichfinal << "       Black Hole" << endl ;
+    fichfinal <<
+      "==================================================================="
+	      << endl ;
+    fichfinal << "Grid : " << endl ;
+    fichfinal << "------ " << endl ;
+    fichfinal << *((bibi.get_bh()).get_mp().get_mg()) << endl ;
+    // No output for black hole
+    /*
+    fichfinal << endl << "Physical characteristics : " << endl ;
+    fichfinal	  << "-------------------------" << endl ;
+    fichfinal << bibi.get_bh() << endl ;
+    */
+    fichfinal << endl ;
+
+    fichfinal << endl <<
+      "==================================================================="
+	      << endl ;
+    fichfinal << "Diff_ent :  neutron star : " << differ[0](0) << endl ;
+    fichfinal << "dH/dx at r = 0 :  star 1 : " << dentdx << endl ;
+
+    fichfinal << endl <<
+      "================================================================"
+	      << endl ;
+    fichfinal << "	    PARAMETERS USED FOR THE COMPUTATION : " << endl ;
+    fichfinal <<
+      "================================================================"
+	      << endl ;
+    fichfinal.close() ;
+    system("cat parcoal.d >> calcul.d") ;
+
+    // Identification du code et de ses sous-routines (no. de version RCS) :
+
+    fichfinal.open("calcul.d", ios::app ) ;
+    fichfinal << endl <<
+      "================================================================"
+	      << endl ;
+    fichfinal << "	    IDENTIFICATION OF THE CODE : " << endl ;
+    fichfinal <<
+      "================================================================"
+	      << endl ;
+    fichfinal.close() ;
+    system("ident coal >> calcul.d") ;
+
+
+    // Cleaning
+    // --------
+
+    delete peos ;
+
+    return EXIT_SUCCESS ;
 
 }
