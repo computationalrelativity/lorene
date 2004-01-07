c048f9cd375673d3ca00ae3ddb6fe52c37906053
p_grandclement <p_grandclement>
2004-01-07 14:36:38 +0000

    Modif mineure in Param_elliptic.set_variable

--- C++/Source/Param_elliptic/param_elliptic.C
+++ C++/Source/Param_elliptic/param_elliptic.C
@@ -23,6 +23,9 @@ char param_elliptic_C[] = "$Header$" ;
 /*
  * $Id$
  * $Log$
+ * Revision 1.4  2004/01/07 14:36:38  p_grandclement
+ * Modif mineure in Param_elliptic.set_variable
+ *
  * Revision 1.3  2003/12/11 16:11:38  e_gourgoulhon
  * Changed #include <iostream.h> to #include "headcpp.h".
  *
@@ -218,8 +221,10 @@ void Param_elliptic::set_variable (int type_variable, int zone) {
     
     for (int k=0 ; k<np+1 ; k++)
       for (int j=0 ; j<nt ; j++) {
-	if ((variables[conte] != 0x0) && (l==zone) && (k==0) && (j==0))
+	if ((variables[conte] != 0x0) && (l==zone) && (k==0) && (j==0)) {
+	  delete variables[conte] ;
 	  variables[conte] = new Change_var(type_variable) ;
+	}
 	conte ++ ;
       }
   }
