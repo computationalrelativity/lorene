c72e2cbc844062e9b9627aab2c6f3f2d87609210
e_gourgoulhon <e_gourgoulhon>
2004-02-05 13:46:21 +0000

    Take into account the new dzpuis conventions for derivatives. Tests passed in the generic case with htt dzpuis = 0, 2, 3 and 4.

--- Codes/Test/Tensor/test_sym_tensor_tt.C
+++ Codes/Test/Tensor/test_sym_tensor_tt.C
@@ -4,7 +4,7 @@
  */
 
 /*
- *   Copyright (c) 2003 Eric Gourgoulhon & Jerome Novak
+ *   Copyright (c) 2003-2004 Eric Gourgoulhon & Jerome Novak
  *
  *   This file is part of LORENE.
  *
@@ -28,6 +28,10 @@ char test_sym_tensor_tt_C[] = "$Header$" ;
 /*
  * $Id$
  * $Log$
+ * Revision 1.11  2004/02/05 13:46:21  e_gourgoulhon
+ * Take into account the new dzpuis conventions for derivatives.
+ * Tests passed in the generic case with htt dzpuis = 0, 2, 3 and 4.
+ *
  * Revision 1.10  2003/11/06 10:33:41  e_gourgoulhon
  * Added tests for the mu part.
  *
@@ -77,254 +81,265 @@ char test_sym_tensor_tt_C[] = "$Header$" ;
 
 int main() {
 
-	// Construction of a multi-grid (Mg3d)
-	// -----------------------------------
+    // Construction of a multi-grid (Mg3d)
+    // -----------------------------------
   
-	int nz = 3 ; 	// Number of domains
-	int nzm1 = nz - 1 ;  
-	int nr = 9 ; 	// Number of collocation points in r in each domain
-	int nt = 7 ; 	// Number of collocation points in theta in each domain
-	int np = 16 ; 	// Number of collocation points in phi in each domain
-	int symmetry_theta = SYM ; // symmetry with respect to the equatorial plane
-	int symmetry_phi = NONSYM ; // no symmetry in phi
-	bool compact = true ; // external domain is compactified
+    int nz = 3 ;    // Number of domains
+    int nzm1 = nz - 1 ;  
+    int nr = 9 ;    // Number of collocation points in r in each domain
+    int nt = 7 ;    // Number of collocation points in theta in each domain
+    int np = 16 ;   // Number of collocation points in phi in each domain
+    int symmetry_theta = SYM ; // symmetry with respect to the equatorial plane
+    int symmetry_phi = NONSYM ; // no symmetry in phi
+    bool compact = true ; // external domain is compactified
   
-	Mg3d mgrid(nz, nr, nt, np, symmetry_theta, symmetry_phi, compact) ;
-	
-	
-  	// Construction of an affine mapping (Map_af)
-  	// ------------------------------------------
+    Mg3d mgrid(nz, nr, nt, np, symmetry_theta, symmetry_phi, compact) ;
+    
+    
+    // Construction of an affine mapping (Map_af)
+    // ------------------------------------------
 
-	// Boundaries of each domains
-	double r_limits[] = {0., 2., 3., __infinity} ; 
-  	assert( nz == 3 ) ;  // since the above array described only 3 domains
+    // Boundaries of each domains
+    double r_limits[] = {0., 2., 3., __infinity} ; 
+    assert( nz == 3 ) ;  // since the above array described only 3 domains
   
-	Map_af map(mgrid, r_limits) ; 
-  	
+    Map_af map(mgrid, r_limits) ; 
+    
 
-	// Construction of a flat metric
-	// -----------------------------
+    // Construction of a flat metric
+    // -----------------------------
 
-	Metric_flat mets(map, map.get_bvect_spher()) ; // spherical representation
-	Metric_flat metc(map, map.get_bvect_cart()) ;  // Cartesian representation
+    Metric_flat mets(map, map.get_bvect_spher()) ; // spherical representation
+    Metric_flat metc(map, map.get_bvect_cart()) ;  // Cartesian representation
 
 
-	// Construction of a divergence free tensor field 
-	// ----------------------------------------------
+    // Construction of a divergence free tensor field 
+    // ----------------------------------------------
 
-	const Coord& x = map.x ; 
-	const Coord& y = map.y ; 
-	const Coord& z = map.z ; 
-	const Coord& r = map.r ; 
-	const Coord& cost = map.cost ; 
-	const Coord& sint = map.sint ; 
-	const Coord& cosp = map.cosp ; 
-	const Coord& sinp = map.sinp ; 
-	const Coord& phi = map.phi ; 
+    const Coord& x = map.x ; 
+    const Coord& y = map.y ; 
+    const Coord& z = map.z ; 
+    const Coord& r = map.r ; 
+    const Coord& cost = map.cost ; 
+    const Coord& sint = map.sint ; 
+    const Coord& cosp = map.cosp ; 
+    const Coord& sinp = map.sinp ; 
+    const Coord& phi = map.phi ; 
 
-	
-	cout << "========================================================" << endl ;
-	cout << "                Test with a constant tensor" << endl ;
-	cout << "   Cart. comp. h^{ij} = (0, 1, 2), (2,-1,3), (2,3,1) " << endl ; 
-	cout << "========================================================" << endl ;
+    
+    cout << "========================================================" << endl ;
+    cout << "                Test with a constant tensor" << endl ;
+    cout << "   Cart. comp. h^{ij} = (0, 1, 2), (2,-1,3), (2,3,1) " << endl ; 
+    cout << "========================================================" << endl ;
 
-	Sym_tensor_trans hhc(map, map.get_bvect_cart(), metc ) ; 
-	
-	hhc.set(1,1) = 1 ; 
-	hhc.set(1,2) = 2 ; 
-	hhc.set(1,3) = 0 ; 
-	hhc.set(2,2) = -4 ; 
-	hhc.set(2,3) = 0 ; 
-	hhc.set(3,3) = 3 ; 
-	hhc.std_spectral_base() ; 
+    Sym_tensor_trans hhc(map, map.get_bvect_cart(), metc ) ; 
+    
+    hhc.set(1,1) = 1 ; 
+    hhc.set(1,2) = 2 ; 
+    hhc.set(1,3) = 0 ; 
+    hhc.set(2,2) = -4 ; 
+    hhc.set(2,3) = 0 ; 
+    hhc.set(3,3) = 3 ; 
+    hhc.std_spectral_base() ; 
 
-	cout << "Cartesian components : hhc : " << endl ;
-	hhc.spectral_display() ; 
-	arrete() ; 
+    cout << "Cartesian components : hhc : " << endl ;
+    hhc.spectral_display() ; 
+    arrete() ; 
 
-	Tensor tmp = hhc ; 
-	tmp.change_triad( map.get_bvect_spher() ) ; 
-	Sym_tensor_trans hhs(map, map.get_bvect_spher(), mets ) ; 
-	hhs = tmp ; 
-	
-	cout << "Spherical components : hhs : " << endl ;
-	hhs.spectral_display() ; 
-	arrete() ; 
-	
-	cout << "Maxabs divergence hhc : " << endl ; 
-	maxabs( hhc.divergence(metc) ) ; 
-	cout << "Maxabs divergence hhs : " << endl ; 
-	maxabs( hhs.divergence(mets) ) ; 
-		
-	Scalar trace =  hhc.trace() ; 
-	cout << "Maxabs Trace of hhc : " << endl ; 	
-	maxabs( trace ) ; 	
-		
-	cout << "Maxabs Trace of hhs : " << endl ; 	
-	maxabs( hhs.trace() ) ; 
+    Tensor tmp = hhc ; 
+    tmp.change_triad( map.get_bvect_spher() ) ; 
+    Sym_tensor_trans hhs(map, map.get_bvect_spher(), mets ) ; 
+    hhs = tmp ; 
+    
+    cout << "Spherical components : hhs : " << endl ;
+    hhs.spectral_display() ; 
+    arrete() ; 
+    
+    cout << "Maxabs divergence hhc : " << endl ; 
+    maxabs( hhc.divergence(metc) ) ; 
+    cout << "Maxabs divergence hhs : " << endl ; 
+    maxabs( hhs.divergence(mets) ) ; 
+        
+    Scalar trace =  hhc.trace() ; 
+    cout << "Maxabs Trace of hhc : " << endl ;  
+    maxabs( trace ) ;   
+        
+    cout << "Maxabs Trace of hhs : " << endl ;  
+    maxabs( hhs.trace() ) ; 
 
-	cout << "========================================================" << endl ;
-	cout << "                Test with the tensor" << endl ;
-	cout << "   Cart. comp. h^{ij} = d_i d_j Phi  with Lap(Phi) = 0 " << endl ; 
-	cout << "========================================================" << endl ;
-	
-	arrete() ; 
-	
-	Scalar pot(map) ; 
-	pot =  1 					// P_0^0
-		   + x					// P_1^1 cos(p)
-		   + y					// P_1^1 sin(p)
-		   + (3*z*z - r*r) 		// P_2^0
-		   + (x*x - y*y ) 		// P_2^2 cos(2p)
-		   + x*y 	 			// P_2^2 sin(2p)
-		   + x*(5*z*z-r*r)		// P_3^1 cos(p)
-		   + y*(5*z*z-r*r)		// P_3^1 sin(p)
-		   + x*(x*x-3*y*y)		// P_3^3 cos(3p)
-		   + y*(y*y-3*x*x) ;	// P_3^3 sin(3p)
-		   
-	pot.annule_domain(nzm1) ; 
+    cout << "========================================================" << endl ;
+    cout << "                Test with the tensor" << endl ;
+    cout << "   Cart. comp. h^{ij} = d_i d_j Phi  with Lap(Phi) = 0 " << endl ; 
+    cout << "========================================================" << endl ;
+    
+    arrete() ; 
+    
+    Scalar pot(map) ; 
+    pot =  1                    // P_0^0
+           + x                  // P_1^1 cos(p)
+           + y                  // P_1^1 sin(p)
+           + (3*z*z - r*r)      // P_2^0
+           + (x*x - y*y )       // P_2^2 cos(2p)
+           + x*y                // P_2^2 sin(2p)
+           + x*(5*z*z-r*r)      // P_3^1 cos(p)
+           + y*(5*z*z-r*r)      // P_3^1 sin(p)
+           + x*(x*x-3*y*y)      // P_3^3 cos(3p)
+           + y*(y*y-3*x*x) ;    // P_3^3 sin(3p)
+           
+    pot.annule_domain(nzm1) ; 
 
-	Mtbl potced = 1 / r 		// P_0^0
-		   + sint*cosp / pow(r,2)	// P_1^1 cos(p)
-		   + sint*sinp / pow(r,2)	// P_1^1 sin(p)
-		   + (3*cost*cost - 1) / pow(r,3)			// P_2^0
-		   + sint*sint*cos(2*phi) / pow(r,3)		// P_2^2 cos(2p)
-		   + sint*sint*sin(2*phi) / pow(r,3)	 	// P_2^2 sin(2p)
-		   + sint*(15*cost*cost-3)*cosp / pow(r,4)		// P_3^1 cos(p)
-		   + sint*(15*cost*cost-3)*sinp / pow(r,4)		// P_3^1 sin(p)
-		   + pow(sint,3) * cos(3*phi) / pow(r,4)		// P_3^3 cos(3p)
-		   + pow(sint,3) * sin(3*phi) / pow(r,4) ;		// P_3^3 sin(3p)
-		   
-	pot.set_domain(nzm1) = potced(nzm1) ; 
-	pot.std_spectral_base() ; 
+    Mtbl potced = 1 / r         // P_0^0
+           + sint*cosp / pow(r,2)   // P_1^1 cos(p)
+           + sint*sinp / pow(r,2)   // P_1^1 sin(p)
+           + (3*cost*cost - 1) / pow(r,3)           // P_2^0
+           + sint*sint*cos(2*phi) / pow(r,3)        // P_2^2 cos(2p)
+           + sint*sint*sin(2*phi) / pow(r,3)        // P_2^2 sin(2p)
+           + sint*(15*cost*cost-3)*cosp / pow(r,4)      // P_3^1 cos(p)
+           + sint*(15*cost*cost-3)*sinp / pow(r,4)      // P_3^1 sin(p)
+           + pow(sint,3) * cos(3*phi) / pow(r,4)        // P_3^3 cos(3p)
+           + pow(sint,3) * sin(3*phi) / pow(r,4) ;      // P_3^3 sin(3p)
+           
+    pot.set_domain(nzm1) = potced(nzm1) ; 
+    pot.std_spectral_base() ; 
 
-	cout << "Potential : " << endl ; 
-	pot.spectral_display() ; 
-	arrete() ; 
-	
-	cout << "Laplacian of potential : " << endl ; 
-	pot.laplacian().spectral_display() ; 
-	arrete() ; 
-	
-	hhc.set(1,1) = pot.dsdx(0).dsdx(0) ; 
-	hhc.set(1,2) = pot.dsdx(0).dsdy(0) ; 
-	hhc.set(1,3) = pot.dsdx(0).dsdz(0) ; 
-	hhc.set(2,2) = pot.dsdy(0).dsdy(0) ; 
-	hhc.set(2,3) = pot.dsdy(0).dsdz(0) ; 
-	hhc.set(3,3) = pot.dsdz(0).dsdz(0) ; 
+    cout << "Potential : " << endl ; 
+    pot.spectral_display() ; 
+    arrete() ; 
+    
+    cout << "Laplacian of potential : " << endl ; 
+    pot.laplacian().spectral_display() ; 
+    arrete() ; 
+    
+    hhc.set(1,1) = pot.dsdx().dsdx() ; 
+    hhc.set(1,2) = pot.dsdx().dsdy() ; 
+    hhc.set(1,3) = pot.dsdx().dsdz() ; 
+    hhc.set(2,2) = pot.dsdy().dsdy() ; 
+    hhc.set(2,3) = pot.dsdy().dsdz() ; 
+    hhc.set(3,3) = pot.dsdz().dsdz() ; 
 
-	cout << "Cartesian components : hhc : " <<  endl ;
-	hhc.spectral_display() ; 
-	arrete() ; 
+    // hhc.dec_dzpuis(1) ; 
 
-	tmp = hhc ; 
-	tmp.change_triad( map.get_bvect_spher() ) ; 
-	hhs = tmp ; 
-	
-	cout << "Spherical components : hhs : " << endl ;
-	hhs.spectral_display() ; 
-	arrete() ; 
-	
-	Vector divc = hhc.divergence(metc) ; 
-	cout << "Divergence of hhc : " << endl ; 
-	divc.spectral_display() ; 
-	
-	cout << "Maxabs divergence hhc : " << endl ; 
-	maxabs( divc ) ; 
-	
-	arrete() ;
-	
-	Vector divs = hhs.divergence(mets) ; 
-	cout << "Divergence of hhs : " << endl ; 
-	divs.spectral_display() ; 
-	
-	cout << "Maxabs divergence hhs : " << endl ; 
-	maxabs( divs ) ; 
-		
-	cout << "Maxabs of trace of hhc : " << endl ; 
-	maxabs( hhc.trace() ) ; 
-		
-	cout << "Maxabs of trace of hhs : " << endl ; 
-	maxabs( hhs.trace() ) ; 
-		
-	arrete() ; 
-	
-	Sym_tensor_tt  htt(map, map.get_bvect_spher(), mets ) ; 
-	
-	htt = hhs ; 
-	
-	cout << "Eta : " << endl ; 
-	htt.eta().spectral_display() ; 
+    cout << "Cartesian components : hhc : " <<  endl ;
+    hhc.spectral_display() ; 
+    arrete() ; 
 
-	cout << "Mu : " << endl ; 
-	htt.mu().spectral_display() ; 
-	
-	
-	arrete() ; 
-	
-	Sym_tensor_tt htt2(map, map.get_bvect_spher(), mets ) ; 
-		
-	htt2.set_rr_eta_mu(htt(1,1), htt.eta(), htt.mu()) ; 
+    tmp = hhc ; 
+    tmp.change_triad( map.get_bvect_spher() ) ; 
+    hhs = tmp ; 
+    
+    cout << "Spherical components : hhs : " << endl ;
+    hhs.spectral_display() ; 
+    arrete() ; 
+    
+    Sym_tensor hhs2 = pot.derive_con(mets).derive_con(mets) ; 
+    // hhs2.dec_dzpuis(1) ; 
+    
+    cout << "### Difference between hhs2 and hhs" << endl ; 
+    maxabs( hhs2 - hhs ) ; 
+    arrete() ; 
+    
+    
+    Vector divc = hhc.divergence(metc) ; 
+    cout << "Divergence of hhc : " << endl ; 
+    divc.spectral_display() ; 
+    
+    cout << "Maxabs divergence hhc : " << endl ; 
+    maxabs( divc ) ; 
+    
+    arrete() ;
+    
+    Vector divs = hhs.divergence(mets) ; 
+    cout << "Divergence of hhs : " << endl ; 
+    divs.spectral_display() ; 
+    
+    cout << "Maxabs divergence hhs : " << endl ; 
+    maxabs( divs ) ; 
+        
+    cout << "Maxabs of trace of hhc : " << endl ; 
+    maxabs( hhc.trace() ) ; 
+        
+    cout << "Maxabs of trace of hhs : " << endl ; 
+    maxabs( hhs.trace() ) ; 
+        
+    arrete() ; 
+    
+    Sym_tensor_tt  htt(map, map.get_bvect_spher(), mets ) ; 
+    
+    htt = hhs ; 
+    
+    cout << "Eta : " << endl ; 
+    htt.eta().spectral_display() ; 
 
-	cout << "Relative difference (max) between htt2 and htt : " << endl ; 
-	diffrelmax(htt2, htt) ; 
+    cout << "Mu : " << endl ; 
+    htt.mu().spectral_display() ; 
+    
+    
+    arrete() ; 
+    
+    Sym_tensor_tt htt2(map, map.get_bvect_spher(), mets ) ; 
+        
+    htt2.set_rr_eta_mu(htt(1,1), htt.eta(), htt.mu()) ; 
 
-	cout << endl << "========================================================" << endl ;
-	cout << "                Test with an ad hoc mu" << endl ;
-	cout << "========================================================" << endl ;
-	
-	arrete() ; 
-	
-	Scalar mu0(map) ; 
-	mu0 = z * pot.get_spectral_va() ;
-		   
-	mu0.annule_domain(nzm1) ; 
+    cout << "Relative difference (max) between htt2 and htt : " << endl ; 
+    diffrelmax(htt2, htt) ; 
 
-	Mtbl mu0ced = cost * potced  ;	
-		   
-	mu0.set_domain(nzm1) = mu0ced(nzm1) ; 
-	mu0.std_spectral_base() ; 
-	mu0.set_spectral_va().set_base_r(0, R_CHEBPIM_I) ; 
-	mu0.set_spectral_va().set_base_t(T_COSSIN_CI) ; 
-	
+    cout << endl << "========================================================" << endl ;
+    cout << "                Test with an ad hoc mu" << endl ;
+    cout << "========================================================" << endl ;
+    
+    arrete() ; 
+    
+    Scalar mu0(map) ; 
+    mu0 = z * pot.get_spectral_va() ;
+           
+    mu0.annule_domain(nzm1) ; 
 
-	cout << "mu0 : " << endl ; 
-	mu0.spectral_display() ; 
-	arrete() ; 
+    Mtbl mu0ced = cost * potced  ;  
+           
+    mu0.set_domain(nzm1) = mu0ced(nzm1) ; 
+    mu0.std_spectral_base() ; 
+    mu0.set_spectral_va().set_base_r(0, R_CHEBPIM_I) ; 
+    mu0.set_spectral_va().set_base_t(T_COSSIN_CI) ; 
+    mu0.set_dzpuis( htt.eta().get_dzpuis() ) ; 
+    
 
-	Sym_tensor_tt htt3 = htt ; 
-		
-	htt3.set_rr_mu(htt(1,1), mu0) ; 
+    cout << "mu0 : " << endl ; 
+    mu0.spectral_display() ; 
+    arrete() ; 
 
-	cout << "Eta htt3 : " << endl ; 
-	htt3.eta().spectral_display() ; 
+    Sym_tensor_tt htt3 = htt ; 
+        
+    htt3.set_rr_mu(htt(1,1), mu0) ; 
 
-	cout << "Mu htt3 : " << endl ; 
-	htt3.mu().spectral_display() ; 
-	
-	arrete() ; 
+    cout << "Eta htt3 : " << endl ; 
+    htt3.eta().spectral_display() ; 
 
-	cout << "htt3 : " << endl ; 
-	htt3.spectral_display() ; 
-	arrete() ; 
+    cout << "Mu htt3 : " << endl ; 
+    htt3.mu().spectral_display() ; 
+    
+    arrete() ; 
 
-	Sym_tensor_tt htt4(map, map.get_bvect_spher(), mets ) ; 
-		
-	htt4.set_rr_eta_mu(htt3(1,1), htt3.eta(), htt3.mu()) ; 
+    cout << "htt3 : " << endl ; 
+    htt3.spectral_display() ; 
+    arrete() ; 
 
-	cout << "Relative difference (max) between htt4 and htt3 : " << endl ; 
-	diffrelmax(htt4, htt3) ; 
+    Sym_tensor_tt htt4(map, map.get_bvect_spher(), mets ) ; 
+        
+    htt4.set_rr_eta_mu(htt3(1,1), htt3.eta(), htt3.mu()) ; 
 
-	arrete() ; 
+    cout << "Relative difference (max) between htt4 and htt3 : " << endl ; 
+    diffrelmax(htt4, htt3) ; 
 
-	Sym_tensor_tt htt5 = htt3 ; 
-	htt5.set(1,1) = htt3(1,1) ; // To force the deletion of eta and mu
+    arrete() ; 
 
-	cout << "Relative difference (max) between htt5.eta and htt3.eta : " << endl ; 
-	diffrelmax(htt5.eta(), htt3.eta(), cout) ; 
-	cout << "Relative difference (max) between htt5.mu and htt3.mu : " << endl ; 
-	diffrelmax(htt5.mu(), htt3.mu(), cout) ; 
+    Sym_tensor_tt htt5 = htt3 ; 
+    htt5.set(1,1) = htt3(1,1) ; // To force the deletion of eta and mu
 
-		
-	return EXIT_SUCCESS ; 
+    cout << "Relative difference (max) between htt5.eta and htt3.eta : " << endl ; 
+    diffrelmax(htt5.eta(), htt3.eta(), cout) ; 
+    cout << "Relative difference (max) between htt5.mu and htt3.mu : " << endl ; 
+    diffrelmax(htt5.mu(), htt3.mu(), cout) ; 
+
+        
+    return EXIT_SUCCESS ; 
 }
