af91b6e62342edf724c21cb97e8a10a6f74fc68a
e_gourgoulhon <e_gourgoulhon>
2004-02-17 22:16:08 +0000

    First version

---
+++ C++/Source/Non_class_members/Graphics/des_evolution.C
@@ -0,0 +1,85 @@
+/*
+ *  Plot of time evolution
+ *
+ *    (see file graphique.h for documentation).
+ *
+ */
+
+/*
+ *   Copyright (c) 2004  Eric Gourgoulhon. 
+ *
+ *   This file is part of LORENE.
+ *
+ *   LORENE is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License version 2
+ *   as published by the Free Software Foundation.
+ *
+ *   LORENE is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with LORENE; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+char des_evolution_C[] = "$Header$" ;
+
+/*
+ * $Id$
+ * $Log$
+ * Revision 1.1  2004/02/17 22:16:08  e_gourgoulhon
+ * First version
+ *
+ *
+ * $Header$
+ *
+ */
+
+
+// Lorene headers
+#include "graphique.h"
+#include "evolution.h"
+
+
+void des_evol(const Evolution<double>& uu, int j_min, int j_max, 
+    bool closeit, const char* nomy, const char* title, bool show_time,
+    int ngraph, const char* nomx) 
+{
+
+    int npt = j_max - j_min + 1 ; 
+
+    float* uutab = new float[npt] ;	    // Values of uu at the npt points
+    float* xtab = new float[npt] ;	    // Values of t at the npt points
+    
+    for (int j=j_min; j<=j_max; j++) {
+	uutab[j] = uu[j] ; 
+    }
+
+    if (show_time) {
+        for (int j=j_min; j<=j_max; j++) {
+            xtab[j] = uu.get_time(j) ; 
+        }
+    }
+    else{
+        for (int j=j_min; j<=j_max; j++) {
+            xtab[j] = j ; 
+        }
+    }
+        
+    if (nomx == 0x0) nomx = (show_time) ? "t" : "j" ;
+
+    if (nomy == 0x0) nomy = "" ;
+
+    if (title == 0x0) title = "" ;
+   
+    
+    des_profile_mult(uutab, 1, npt, xtab, nomx, nomy, title, 0x0, ngraph,
+        closeit) ; 
+    
+    delete [] uutab ; 
+    
+}
+
