b2974288dbc9c6d04532217957be5096531bd06e
f_limousin <f_limousin>
2004-03-31 11:18:42 +0000

    Methods Map_et::poisson_interne, Map_af::poisson_interne and Cmp::poisson_neumann_interne have been implemented to solve the continuity equation for strange stars.

--- C++/Source/Cmp/cmp_pde_frontiere.C
+++ C++/Source/Cmp/cmp_pde_frontiere.C
@@ -25,6 +25,11 @@ char cmp_pde_frontiere_C[] = "$Header$" ;
 /*
  * $Id$
  * $Log$
+ * Revision 1.3  2004/03/31 11:18:42  f_limousin
+ * Methods Map_et::poisson_interne, Map_af::poisson_interne and
+ * Cmp::poisson_neumann_interne have been implemented to solve the
+ * continuity equation for strange stars.
+ *
  * Revision 1.2  2003/10/03 15:58:45  j_novak
  * Cleaning of some headers
  *
@@ -58,6 +63,8 @@ char cmp_pde_frontiere_C[] = "$Header$" ;
  */
 
 // Header Lorene:
+#include "scalar.h" 
+#include "param.h" 
 #include "cmp.h"
 
 Mtbl_cf sol_poisson_frontiere(const Map_af&, const Mtbl_cf&, const Mtbl_cf&,
@@ -65,6 +72,8 @@ Mtbl_cf sol_poisson_frontiere(const Map_af&, const Mtbl_cf&, const Mtbl_cf&,
 
 Mtbl_cf sol_poisson_frontiere_double (const Map_af&, const Mtbl_cf&, const Mtbl_cf&,
 				    const Mtbl_cf&, int) ;
+
+Mtbl_cf sol_poisson_interne(const Map_af&, const Mtbl_cf&, const Mtbl_cf&) ;
 
 Cmp Cmp::poisson_dirichlet (const Valeur& limite, int num_front) const {
     
@@ -78,6 +87,13 @@ Cmp Cmp::poisson_neumann (const Valeur& limite, int num_front) const {
     
     Cmp resu(*mp) ;
     mp->poisson_frontiere (*this, limite, 2, num_front, resu) ; 
+    return resu ;    
+}
+
+Cmp Cmp::poisson_neumann_interne (const Valeur& limite, 
+				  Param& par, Cmp& resu) const {
+    
+    mp->poisson_interne (*this, limite, par, resu) ; 
     return resu ;    
 }
 
@@ -105,7 +121,7 @@ void Map_af::poisson_frontiere(const Cmp& source, const Valeur& limite, int type
     assert(source.get_etat() != ETATNONDEF) ; 
     assert(source.get_mp()->get_mg() == mg) ; 
     assert(pot.get_mp()->get_mg() == mg) ; 
-    assert (source.get_mp()->get_mg()->get_angu() == limite.get_mg()) ;
+    assert (source.get_mp()->get_mg() == limite.get_mg()) ;
     
     assert( source.check_dzpuis(2) || source.check_dzpuis(4) 
 	    || source.check_dzpuis(3)) ; 
@@ -226,3 +242,256 @@ void Map_af::poisson_frontiere_double (const Cmp& source, const Valeur& lim_func
     pot.va = resu ;
     (pot.va).ylm_i() ; // On repasse en base standard.
 }
+
+
+
+void Map_et::poisson_interne(const Cmp& source, const Valeur& limite,
+			     Param& par, Cmp& uu) const {
+
+    assert(source.get_etat() != ETATNONDEF) ; 
+    assert(source.get_mp() == this) ;
+
+    assert(uu.get_mp() == this) ; 
+
+    int nz = mg->get_nzone() ; 
+     
+    //-------------------------------
+    // Computation of the prefactor a  ---> Cmp apre
+    //-------------------------------
+
+    Mtbl unjj = 1 + srdrdt*srdrdt + srstdrdp*srstdrdp ;
+
+    Mtbl apre1(*mg) ; 
+    apre1.set_etat_qcq() ; 
+    for (int l=0; l<nz; l++) {
+	*(apre1.t[l]) = alpha[l]*alpha[l] ; 
+    }
+
+    apre1 = apre1 * dxdr * dxdr * unjj ;
+
+    Cmp apre(*this) ; 
+    apre = apre1 ; 
+    
+    Tbl amax0 = max(apre1) ;	// maximum values in each domain
+
+    // The maximum values of a in each domain are put in a Mtbl
+    Mtbl amax1(*mg) ; 
+    amax1.set_etat_qcq() ; 
+    for (int l=0; l<nz; l++) {
+	*(amax1.t[l]) = amax0(l) ; 
+    }
+    
+    Cmp amax(*this) ; 
+    amax = amax1 ; 
+
+    
+    //-------------------
+    //  Initializations 
+    //-------------------
+    
+    int nitermax = par.get_int() ; 
+    int& niter = par.get_int_mod() ; 
+    double lambda = par.get_double() ; 
+    double unmlambda = 1. - lambda ; 
+    double precis = par.get_double(1) ;     
+    
+    Cmp& ssj = par.get_cmp_mod() ; 
+    
+    Cmp ssjm1 = ssj ; 
+    Cmp ssjm2 = ssjm1 ; 
+
+    Valeur& vuu = uu.va ; 
+
+    Valeur vuujm1(*mg) ;
+    if (uu.get_etat() == ETATZERO) {
+	vuujm1 = 1 ;	// to take relative differences
+	vuujm1.set_base( vuu.base ) ; 
+    }
+    else{
+	vuujm1 = vuu ; 
+    }
+    
+    // Affine mapping for the Laplacian-tilde
+
+    Map_af mpaff(*this) ; 
+    Param par_nul ; 
+
+    cout << "Map_et::poisson : relat. diff. u^J <-> u^{J-1} : " << endl ;
+    
+//==========================================================================
+//==========================================================================
+//			    Start of iteration 
+//==========================================================================
+//==========================================================================
+
+    Tbl tdiff(nz) ; 
+    niter = 0 ; 
+    
+    do {
+
+    //====================================================================
+    //		Computation of R(u)    (the result is put in uu)
+    //====================================================================
+
+
+    //-----------------------
+    // First operations on uu
+    //-----------------------
+    
+    Valeur duudx = (uu.va).dsdx() ;	    // d/dx 
+
+    const Valeur& d2uudtdx = duudx.dsdt() ;	    // d^2/dxdtheta
+
+    const Valeur& std2uudpdx = duudx.stdsdp() ;    // 1/sin(theta) d^2/dxdphi   
+
+    //------------------
+    // Angular Laplacian 
+    //------------------
+    
+    Valeur sxlapang = uu.va ; 
+
+    sxlapang.ylm() ; 
+    
+    sxlapang = sxlapang.lapang() ;    
+    
+    sxlapang = sxlapang.sx() ;	  //  Lap_ang(uu) /x      in the nucleus
+				    
+    //---------------------------------------------------------------
+    //  Computation of 
+    // [ 2 /(dRdx) ( A - 1 ) duu/dx + 1/R (B - 1) Lap_ang(uu) ] / x
+    //
+    // with A = 1/(dRdx) R/(x+beta_l/alpha_l) unjj 
+    //	    B = [1/(dRdx) R/(x+beta_l/alpha_l)]^2 unjj 
+    // 
+    //  The result is put in uu (via vuu)
+    //---------------------------------------------------------------
+
+    Valeur varduudx = duudx ; 
+
+    uu.set_etat_qcq() ; 
+    
+    Base_val sauve_base = varduudx.base ; 
+    
+    vuu = 2. * dxdr * ( rsxdxdr * unjj - 1.) * varduudx 
+		+ ( rsxdxdr*rsxdxdr * unjj - 1.) * xsr * sxlapang ; 
+
+    vuu.set_base(sauve_base) ; 
+
+    vuu = vuu.sx() ; 
+
+    //---------------------------------------
+    // Computation of  R(u)  
+    //
+    //  The result is put in uu (via vuu)
+    //---------------------------------------
+
+
+    sauve_base = vuu.base ; 
+
+    vuu =  xsr * vuu 
+		+ 2. * dxdr * (	sr2drdt * d2uudtdx 
+			      + sr2stdrdp * std2uudpdx ) ;
+
+    vuu += dxdr * ( lapr_tp + dxdr * ( 
+		dxdr* unjj * d2rdx2 
+		- 2. * ( sr2drdt * d2rdtdx  + sr2stdrdp * sstd2rdpdx ) ) 
+				 ) * duudx ;		    
+
+    vuu.set_base(sauve_base) ; 
+
+    //====================================================================
+    //   Computation of the effective source s^J of the ``affine''
+    //     Poisson equation 
+    //====================================================================
+    
+    ssj = lambda * ssjm1 + unmlambda * ssjm2 ; 
+    
+    ssj = ( source + uu + (amax - apre) * ssj ) / amax ; 
+
+    (ssj.va).set_base((source.va).base) ; 
+    
+    //====================================================================
+    //   Resolution of the ``affine'' Poisson equation 
+    //====================================================================
+    
+    mpaff.poisson_interne(ssj, limite, par_nul, uu) ; 
+
+    tdiff = diffrel(vuu, vuujm1) ; 
+
+    cout << "  step " << niter << " :  " ; 
+    cout << tdiff(0) << "  " ;  
+ 
+    cout << endl ; 
+
+    //=================================
+    //  Updates for the next iteration
+    //=================================
+    
+    ssjm2 = ssjm1 ; 
+    ssjm1 = ssj ; 
+    vuujm1 = vuu ; 
+    
+    niter++ ; 
+    
+    }	// End of iteration 
+    while ( (tdiff(0) > precis) && (niter < nitermax) ) ;
+    
+//==========================================================================
+//==========================================================================
+//			    End of iteration 
+//==========================================================================
+//==========================================================================
+
+}
+
+
+void Map_af::poisson_interne(const Cmp& source, const Valeur& limite, 
+			     Param& par, Cmp& pot) const {
+    
+    assert(source.get_etat() != ETATNONDEF) ; 
+    assert(source.get_mp()->get_mg() == mg) ; 
+    assert(pot.get_mp()->get_mg() == mg) ; 
+    assert (source.get_mp()->get_mg()->get_angu() == limite.get_mg()) ;
+ 
+    // Spherical harmonic expansion of the source
+    // ------------------------------------------
+    
+    Valeur sourva = source.va ;
+    sourva.coef() ;
+    sourva.ylm() ;
+    
+    // Pour gerer le cas ou source est dans ETAT_ZERO...
+    Mtbl_cf so_cf (sourva.get_mg(), sourva.base) ;
+    if (sourva.get_etat() == ETATZERO) {
+	so_cf.set_etat_zero() ;
+	}
+    else
+	so_cf = *sourva.c_cf ;
+    
+    Valeur conditions (limite) ;
+    conditions.coef() ;
+    conditions.ylm() ; // spherical harmonic transforms 
+ 
+
+    // Pour gerer le cas ou condition est dans ETAT_ZERO...
+    Mtbl_cf auxiliaire (conditions.get_mg(), conditions.base) ;
+    if (conditions.get_etat() == ETATZERO)
+	auxiliaire.set_etat_zero() ;
+    else
+	auxiliaire = *conditions.c_cf ;
+
+    // Call to the Mtbl_cf version
+    // ---------------------------
+    
+    Mtbl_cf resu = sol_poisson_interne(*this, so_cf, auxiliaire) ;
+    
+    // Final result returned as a Cmp
+    // ------------------------------
+    
+    pot.set_etat_zero() ;  // to call Cmp::del_t().
+    pot.set_etat_qcq() ;  
+    pot.va = resu ;
+    (pot.va).ylm_i() ; // On repasse en base standard.	    
+    pot.set_dzpuis(0) ; 
+}
+
