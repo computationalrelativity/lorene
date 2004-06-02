05911a778013e16852c741111c08998f4bbd857b
e_gourgoulhon <e_gourgoulhon>
2004-06-02 21:35:59 +0000

    Added root name for OpenDX files.

--- Codes/Einstein/par_visu_evol.d
+++ Codes/Einstein/par_visu_evol.d
@@ -1,12 +1,14 @@
 ########## PARAMETERS FOR CODE VISU_EVOL ########################
-# The following line contains the root for the data file names:
+# The following line contains the root for the input data file names:
 sigma
 2       jmin: initial time step
-6       jmax: final time step
-2       jstep: distance between two successive time steps
+200     jmax: final time step
+20      jstep: distance between two successive time steps
+# The following line contains the root for the OpenDX file names to be created:
+mu
 z       section_type: z (resp. x, y) for a plane z=a (resp. x=a, y=a) with a=const,  
 1.       parameter a defining the section plane (see above)
--10. 10.    umin, umax: range of the plane coordinate u
--10. 10.    vmin, vmax: range of the plane coordinate v
+-4. 4.    umin, umax: range of the plane coordinate u
+-4. 4.    vmin, vmax: range of the plane coordinate v
 200     nu : number of points in the u direction (uniform sampling)
 200     nv : number of points in the v direction (uniform sampling)
