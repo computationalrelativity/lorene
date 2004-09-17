6122c5b4e59430838278aa2c1327c97df50459f9
f_limousin <f_limousin>
2004-09-17 13:34:12 +0000

    *** empty log message ***

---
+++ Codes/Isol_hor/par_hor.d
@@ -0,0 +1,10 @@
+# Parameters for the computation 
+###############################
+4	nz: total number of domains
+9	nt: number of points in theta (the same in each domain)
+8	np: number of points in phi   (the same in each domain)
+25	nr: number of points in r   (the same in each domain)
+1.	coordinate radius
+0.6	relaxation (1 -> no relaxation)
+1e-7	threshold
+
