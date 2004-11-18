254db70b987f48075237e97d417a02cbe34b7b9c
jl_jaramillo <jl_jaramillo>
2004-11-18 09:49:44 +0000

    Some new conditions for the shift (Neumann + Dirichlet)

--- C++/Source/Isol_hor/bound_hor.C
+++ C++/Source/Isol_hor/bound_hor.C
@@ -30,6 +30,9 @@ char bound_hor_C[] = "$Header$" ;
 /*
  * $Id$
  * $Log$
+ * Revision 1.9  2004/11/18 09:49:44  jl_jaramillo
+ * Some new conditions for the shift (Neumann + Dirichlet)
+ *
  * Revision 1.8  2004/11/05 10:52:26  f_limousin
  * Replace double aa by double cc in argument of boundary_beta_x
  * boundary_beta_y and boundary_beta_z to avoid warnings.
@@ -580,3 +583,205 @@ Vector Isol_hor::beta_bound_cart(double velang) {
   return tmp_vect ;
 
 }
+
+
+// Neumann boundary condition for b_tilde 
+//---------------------------------------
+// ONE HAS TO GUARANTEE THAT BETA IS NOT ZERO, BUT IT IS PROPORTIONAL TO THE RADIAL VECTOR
+
+Valeur Isol_hor::boundary_b_tilde_Neu(){
+
+  const Map& map = ff.get_mp() ; 
+  
+  // Introduce 2-trace gamma tilde dot
+
+  Vector s_tilde = met_gamt.radial_vect() ;
+
+  Scalar hh_tilde = contract(s_tilde.derive_cov(met_gamt), 0, 1) ;
+
+  //  des_profile(hh_tilde, 1.00001, 10, M_PI/2., 0., "H_tilde") ;
+
+  Scalar tmp (map) ;
+
+  tmp = b_tilde() * hh_tilde - 2 * ( s_tilde(2) * b_tilde().derive_cov(ff)(2)
+				     + s_tilde(3) * b_tilde().derive_cov(ff)(3) ) ;
+  
+  tmp = tmp / (2 *  s_tilde(1) ) ;
+
+
+  // in this case you don't have to substract any value
+ 
+  Valeur b_tilde_bound (map.get_mg()->get_angu() )  ;
+  
+  int nnp = map.get_mg()->get_np(1) ;
+  int nnt = map.get_mg()->get_nt(1) ;
+			
+  b_tilde_bound = 1 ;
+			
+  for (int k=0 ; k<nnp ; k++)
+    for (int j=0 ; j<nnt ; j++)
+      b_tilde_bound.set(0, k, j, 0) = tmp.val_grid_point(1, k, j, 0) ;
+
+  b_tilde_bound.std_base_scal() ;
+  
+  return b_tilde_bound ;
+
+}
+
+
+Valeur Isol_hor::boundary_b_tilde_Dir(){
+
+  const Map& map = ff.get_mp() ; 
+
+
+  Vector s_tilde = met_gamt.radial_vect() ;
+
+  Scalar hh_tilde = contract(s_tilde.derive_cov(met_gamt), 0, 1) ;
+
+
+  Scalar tmp = 2 * contract (s_tilde, 0, b_tilde().derive_cov(ff) , 0) ; 
+  //  tmp = tmp / hh_tilde  - 1 ;
+
+  //  des_profile(tmp, 1.00001, 10, M_PI/2., 0., "tmp") ;
+
+
+  // We have substracted 1, since we solve for zero condition at infinity 
+  //and then we add 1 to the solution  
+
+  Valeur b_tilde_bound (map.get_mg()->get_angu() )  ;
+  
+  int nnp = map.get_mg()->get_np(1) ;
+  int nnt = map.get_mg()->get_nt(1) ;
+			
+  b_tilde_bound = 1 ;
+			
+  for (int k=0 ; k<nnp ; k++)
+    for (int j=0 ; j<nnt ; j++)
+      b_tilde_bound.set(0, k, j, 0) = tmp.val_grid_point(1, k, j, 0) ;
+
+  b_tilde_bound.std_base_scal() ;
+
+  return b_tilde_bound ;
+
+}
+
+
+Vector Isol_hor::vv_bound_cart(double velang) {
+
+  const Map& map = ff.get_mp() ; 
+
+  Vector tmp_vect (map, CON, *(ff.get_triad()));
+  tmp_vect.set_etat_zero() ;
+
+  Scalar vel_ang (map) ;  
+  vel_ang = velang ;
+  vel_ang.std_spectral_base() ;
+  vel_ang.mult_rsint() ;
+
+  tmp_vect.set(3) = - vel_ang ;
+
+  tmp_vect.std_spectral_base() ;
+
+  tmp_vect.change_triad(map.get_bvect_cart() ) ;
+  
+  /*
+  Scalar tmp(map) ;
+  
+  tmp = tmp_vect(1) ;
+  des_profile(tmp, 1.00001, 10, M_PI/2.,  M_PI/3. , "Source(1)") ;
+  
+  tmp = tmp_vect(2) ;
+  des_profile(tmp, 1.00001, 10, M_PI/2.,  M_PI/3., "Source(2)") ;
+  
+  tmp = tmp_vect(3) ;
+  des_profile(tmp, 1.00001, 10, M_PI/2.,  M_PI/3., "Source(3)") ;
+  */  
+
+return tmp_vect ;
+
+}
+
+
+
+// Component x of boundary value of V^i 
+//-------------------------------------
+Valeur Isol_hor:: boundary_vv_x(double velang){
+
+  const Map& map = ff.get_mp() ; 
+  
+  int nnp = map.get_mg()->get_np(1) ;
+  int nnt = map.get_mg()->get_nt(1) ;
+
+  //Isol_hor boundary conditions
+  
+  Valeur lim_x (map.get_mg()->get_angu()) ;
+    
+  lim_x = 1 ;   // Why is it necessary this and what it is actually doing?
+  
+  Scalar vv_x = vv_bound_cart(velang)(1) ;
+
+  for (int k=0 ; k<nnp ; k++)
+    for (int j=0 ; j<nnt ; j++)
+      lim_x.set(0, k, j, 0) = vv_x.val_grid_point(1, k, j, 0) ;
+  
+  lim_x.set_base(vv_x.get_spectral_va().get_base()) ;
+
+  return  lim_x ;
+
+
+}
+
+
+// Component y of boundary value of V^i
+//--------------------------------------
+Valeur Isol_hor:: boundary_vv_y(double velang){
+  
+  const Map& map = ff.get_mp() ;
+ 
+  int nnp = map.get_mg()->get_np(1) ;
+  int nnt = map.get_mg()->get_nt(1) ;
+
+  // Isol_hor boundary conditions
+  
+  Valeur lim_y (map.get_mg()->get_angu()) ;
+    
+  lim_y = 1 ;   // Why is it necessary this and what it is actually doing?
+ 
+  Scalar vv_y = vv_bound_cart(velang)(2) ;
+
+  for (int k=0 ; k<nnp ; k++)
+      for (int j=0 ; j<nnt ; j++)
+	  lim_y.set(0, k, j, 0) = vv_y.val_grid_point(1, k, j, 0) ;
+
+  lim_y.set_base(vv_y.get_spectral_va().get_base()) ;
+
+  return  lim_y ;
+}
+
+
+// Component z of boundary value of V^i
+//-------------------------------------
+Valeur Isol_hor:: boundary_vv_z(double velang){
+
+  const Map& map = ff.get_mp() ; 
+
+  int nnp = map.get_mg()->get_np(1) ;
+  int nnt = map.get_mg()->get_nt(1) ;
+
+  // Isol_hor boundary conditions
+ 
+  Valeur lim_z (map.get_mg()->get_angu()) ;
+    
+  lim_z = 1 ;   // Why is it necessary this and what it is actually doing?
+  
+  Scalar vv_z = vv_bound_cart(velang)(3) ;
+
+  for (int k=0 ; k<nnp ; k++)
+    for (int j=0 ; j<nnt ; j++)
+      lim_z.set(0, k, j, 0) = vv_z.val_grid_point(1, k, j, 0) ;
+ 
+  lim_z.set_base(vv_z.get_spectral_va().get_base()) ;
+
+  return  lim_z ;
+}
+
