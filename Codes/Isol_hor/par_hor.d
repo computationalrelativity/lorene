2a27cc28f79d3dc8d0112153dafc260aea01e166
jl_jaramillo <jl_jaramillo>
2004-11-24 19:28:38 +0000

    parameters for ih_berlin

--- Codes/Isol_hor/par_hor.d
+++ Codes/Isol_hor/par_hor.d
@@ -1,13 +1,14 @@
 # Parameters for the computation
 ###############################
-8	nz: total number of domains
+6	nz: total number of domains
 9	nt: number of points in theta (the same in each domain)
 8	np: number of points in phi   (the same in each domain)
-33	nr: number of points in r in the first domain
+17	nr: number of points in r in the first domain
 17	nr: number of points in r in all the other domains
 1.	coordinate radius
 0.6	relaxation (1 -> no relaxation)
 1e-10	threshold
-100	maximum of iterations
-0.12	angular velocity
+2000	maximum of iterations
+0.2735	angular velocity
+
 
