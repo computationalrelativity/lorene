7e20066c899bbe7fa5f2c6e4f8388cd4aa1f6880
m_forot <m_forot>
2004-12-17 13:20:55 +0000

    Add T_LEG base

--- C++/Source/Non_class_members/Operators/op_lapang.C
+++ C++/Source/Non_class_members/Operators/op_lapang.C
@@ -39,6 +39,9 @@ char op_lapang_C[] = "$Header$" ;
 /*
  * $Id$
  * $Log$
+ * Revision 1.4  2004/12/17 13:20:55  m_forot
+ * Add T_LEG base
+ *
  * Revision 1.3  2003/09/16 12:11:59  j_novak
  * Added the base T_LEG_II.
  *
@@ -80,6 +83,60 @@ void _lapang_pas_prevu(Mtbl_cf* mt, int l) {
     cout << "Unknwon theta basis in the operator Mtbl_cf::lapang() !" << endl ;
     cout << " basis : " << hex << (mt->base).b[l] << endl ; 
     abort () ;
+}
+			//---------------
+			// cas T_LEG --
+			//---------------
+
+void _lapang_t_leg(Mtbl_cf* mt, int l)
+{
+
+    Tbl* tb = mt->t[l] ;	    // pt. sur tbl de travail
+    
+    // Peut-etre rien a faire ?
+    if (tb->get_etat() == ETATZERO) {
+	return ;
+    }
+    
+    int k, j, i ; 
+    // Pour le confort
+    int nr = mt->get_mg()->get_nr(l) ;   // Nombre
+    int nt = mt->get_mg()->get_nt(l) ;   //	de points
+    int np = mt->get_mg()->get_np(l) ;   //	    physiques
+    
+    int np1 = ( np == 1 ) ? 1 : np+1 ; 
+	
+    double* tuu = tb->t ; 
+
+    // k = 0  :
+     
+    for (j=0 ; j<nt ; j++) {
+	int ll = j ;
+	double xl = - ll*(ll+1) ;
+	for (i=0 ; i<nr ; i++) {
+	    tuu[i] *= xl ;
+	}	// Fin de boucle sur r
+	tuu  += nr ;
+    }     // Fin de boucle sur theta
+
+    // On saute k = 1 : 
+    tuu += nt*nr ; 
+	
+    // k=2,...
+    for (k=2 ; k<np1 ; k++) {
+	int m = k/2 ;
+	tuu  += (m/2)*nr ;
+	for (j=m/2 ; j<nt ; j++) {
+	    int ll = j;
+	    double xl = - ll*(ll+1) ;
+	    for (i=0 ; i<nr ; i++) {
+		tuu[i] *= xl ;
+	    }	// Fin de boucle sur r
+	    tuu  += nr ;
+	}     // Fin de boucle sur theta
+    }	// Fin de boucle sur phi	
+	    
+    // base de developpement inchangee 
 }
 
 			//---------------
