6f8b1c6e14aa51b652c5669386b667038ac75f89
f_limousin <f_limousin>
2004-12-29 17:59:41 +0000

    *** empty log message ***

---
+++ Codes/Bin_hor/par_init.d
@@ -0,0 +1,9 @@
+## Parameters for init_bh 
+#######################################################
+16 		beta
+7		nzones
+17 13 12        points in r theta and phi
+0 1 2 4 5 10 20	bondaries of the domains (no need for infinity)
+1e-7		Convergence treashold
+0.5		Relaxation
+
