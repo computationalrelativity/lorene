c486b3fff22cf76f5eb3dc60871d0aa56f42993b
f_limousin <f_limousin>
2004-12-31 15:45:40 +0000

    *** empty log message ***

--- Codes/Bin_hor/par_init.d
+++ Codes/Bin_hor/par_init.d
@@ -1,9 +1,12 @@
 ## Parameters for init_bh 
 #######################################################
-16 		beta
-7		nzones
-17 13 12        points in r theta and phi
-0 1 2 4 5 10 20	bondaries of the domains (no need for infinity)
-1e-7		Convergence treashold
-0.5		Relaxation
+16 	beta
+5	nz : total number of domains
+7	nt: number of points in theta (the same in each domain)
+8       np: number of points in phi   (the same in each domain)
+9       nr: number of points in r in the first domain
+9       nr: number of points in r in all the other domains
+0 1 2 4 8	boundaries of the domains
+1e-3	Convergence treashold
+0.5	Relaxation
 
