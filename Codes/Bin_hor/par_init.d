7ff960da48327d2269dfeab6d50afe7d3d56e646
f_limousin <f_limousin>
2005-02-25 12:29:46 +0000

    *** empty log message ***

--- Codes/Bin_hor/par_init.d
+++ Codes/Bin_hor/par_init.d
@@ -1,12 +1,30 @@
 ## Parameters for init_bh 
 #######################################################
-16 	beta
-5	nz : total number of domains
+20	beta
+7	nz : total number of domains
 7	nt: number of points in theta (the same in each domain)
-8       np: number of points in phi   (the same in each domain)
+8	np: number of points in phi   (the same in each domain)
 9       nr: number of points in r in the first domain
 9       nr: number of points in r in all the other domains
-0 1 2 4 8	boundaries of the domains
-1e-3	Convergence treashold
+0 1 2 4 8 16 32	boundaries of the domains
+1e-6	Convergence treashold
 0.5	Relaxation
+0 0.	boundary condition for the lapse / value of the coefficient
+0	boundary condition for the psi
 
+########################################################
+For the lapse :
+   0	boundary_nn_Dir(double)
+   1	boundary_nn_Neu_eff(double)
+   2	boundary_nn_Dir_eff(double)   
+   3	boundary_nn_Neu_kk()   
+   4	boundary_nn_Dir_kk()
+   
+For Psi :
+   0	boundary_psi_app_hor()
+   1	boundary_psi_Neu_spat()
+   2	boundary_psi_Dir_spat()
+   3	boundary_psi_Neu_evol()
+   4	boundary_psi_Dir_evol()
+
+    
