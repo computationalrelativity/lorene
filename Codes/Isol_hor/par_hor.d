6b7870934a9e9116f9ae8263d1798c63c5b32118
f_limousin <f_limousin>
2005-03-03 10:16:38 +0000

    *** empty log message ***

--- Codes/Isol_hor/par_hor.d
+++ Codes/Isol_hor/par_hor.d
@@ -7,8 +7,31 @@
 17	nr: number of points in r in all the other domains
 1.	coordinate radius
 0.6	relaxation (1 -> no relaxation)
-1e-10	threshold
-2000	maximum of iterations
-0.2735	angular velocity
+1e-7	threshold
+500	maximum of iterations
+0.01	angular velocity
+0. 0.01	boost velocity in x direction / z direction
+0 0.2	boundary condition for the lapse / value of the coefficient
+1	boundary condition for psi
+0	boundary condition for the shift
 
+########################################################
+For the lapse :
+   0	boundary_nn_Dir(double)
+   1	boundary_nn_Neu_eff(double)
+   2	boundary_nn_Dir_eff(double)   
+   3	boundary_nn_Neu_kk()   
+   4	boundary_nn_Dir_kk()
+   
+For Psi :
+   0	boundary_psi_app_hor()
+   1	boundary_psi_Neu_spat()
+   2	boundary_psi_Dir_spat()
+   3	boundary_psi_Neu_evol()
+   4	boundary_psi_Dir_evol()
+
+For the shift
+   0	boundary_beta_x y,z (coordonnees horizon fixe)
+   1	Berlin boundary condition
+   2	Berlin boundary condition with resolution of the lapse
 
