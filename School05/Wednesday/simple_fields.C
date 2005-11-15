1e3c4d033117b955dcbc3c21f05c6fe7cb8f57b9
j_novak <j_novak>
2005-11-15 17:03:51 +0000

    Working files for wednesday.

---
+++ School05/Wednesday/simple_fields.C
@@ -0,0 +1,12 @@
+// Lorene headers
+#include "metric.h"
+#include "nbr_spx.h"
+#include "utilitaires.h"
+#include "graphique.h"
+
+int main() {
+
+    cout << "All is to be done !" << endl ;
+
+    return EXIT_SUCCESS ; 
+}
