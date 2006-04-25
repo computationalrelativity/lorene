c0656d1d98220d57320840df86e070d70990381c
p_grandclement <p_grandclement>
2006-04-25 07:24:36 +0000

    modification of the parameter structure

--- Codes/Bin_ns_bh/par_coal.d
+++ Codes/Bin_ns_bh/par_coal.d
@@ -1,6 +1,9 @@
-7.5 1.7 #Masses BH NS (in solar mass)
-1.e-3 #Treshold to converge to the masses
-1e-7 #precision
-0.4 # relaxation
+25   # Coordinate distance
+7. 1.2 #Masses BH NS (in solar mass)
+1.e-5 #Treshold to converge to the masses
+1e-8 #precision
+0.6 # relaxation
 1 #max poisson
 4 #autre truc poisson
+4 #number of filtered coefficients
+
