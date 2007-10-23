ccdf9b79a6b1343ebed2bd754aac389eb9dba450
j_novak <j_novak>
2007-10-23 12:26:08 +0000

    Added a test for the case where there is no AH, h(theta,phi) is then going out of the grid

--- C++/Source/App_hor/app_hor_finder.C
+++ C++/Source/App_hor/app_hor_finder.C
@@ -30,6 +30,9 @@ char app_hor_finder_C[] = "$Header$" ;
 /*
  * $Id$
  * $Log$
+ * Revision 1.7  2007/10/23 12:26:08  j_novak
+ * Added a test for the case where there is no AH, h(theta,phi) is then going out of the grid
+ *
  * Revision 1.6  2005/12/09 09:35:06  lm_lin
  *
  * Add more information to screen output if no convergence.
@@ -93,6 +96,9 @@ bool ah_finder(const Metric& gamma, const Sym_tensor& k_dd, Valeur& h, Scalar& e
   const Coord& cosph = map.cosp ;
   const Coord& sinth = map.sint ;
   const Coord& sinph = map.sinp ;
+
+  double r_min = min(+rr)(0) ;
+  double r_max = max(+rr)(nz-1) ;
 
   // Set up a triaxial ellipsoidal surface as the initial guess for h
   //------------------------------------------------------------------
@@ -188,177 +194,184 @@ bool ah_finder(const Metric& gamma, const Sym_tensor& k_dd, Valeur& h, Scalar& e
   Tbl diff_h(nz) ;
   diff_h = 1. ;
 
+  bool no_AH_in_grid = false ;
 
   //--------------------------------------------------------
   //         Start of iteration
   //--------------------------------------------------------
 
-  for (int step=0 ; (max(diff_h) > precis) && (step < step_max) ; step++) {
-
-
-    // ***To be fixed: the function "set_grid_point" does not delete the derived 
-    //                 quantities of F.
-    // Temporary fix: Define the level set F inside the iteration loop...
-    //----------------------------------------------------------------
-
-    Scalar F(map) ;  // level set function: F = r - h(theta,phi) 
-    F.allocate_all() ;
-
-    for (int l=0; l<nz; l++) {
-
-      int imax = mg->get_nr(l) ;
-      int jmax = mg->get_nt(l) ;
-      int kmax = mg->get_np(l) ;
-
-      for (int k=0; k<kmax; k++) {
-	for (int j=0; j<jmax; j++) {
-	  for (int i=0; i<imax; i++) {
-
-	    // (+rr) converts rr to Mtbl
-	    F.set_grid_point(l,k,j,i) = (+rr)(l,k,j,i) - h(l,k,j,0) ; 
-
-	  }
-	}
-      }
-    }
-
-    F.std_spectral_base() ;
-
-    // Construct the unit normal vector s^i of the surface F
-    Scalar dF_norm(map) ;   
-    dF_norm = contract( contract(gamma.con(), 0, F.derive_cov(gamma), 0), 
-			0, F.derive_cov(gamma), 0) ;
-    dF_norm = sqrt( dF_norm ) ;
-    dF_norm.std_spectral_base() ;
-
-    s_unit = F.derive_con(gamma) / dF_norm ; 
-
-    // The expansion function
-    ex_fcn = s_unit.divergence(gamma) - k_dd.trace(gamma) + 
-      contract( s_unit, 0, contract(s_unit, 0, k_dd, 1), 0) ; 
-
-
-    // Construct the source term for the angular Laplace equation
-    //---------------------------------------------------------
-
-
-    
-    Sym_tensor sou_1(map, CON, bspher) ;
-    sou_1 = gamma.con() - fmets.con()/psi4  - s_unit*s_unit  ;
-
-    Scalar source_tmp(map) ;
-    source_tmp = contract( sou_1, 0, 1, F.derive_cov(fmets).derive_cov(fmets), 0, 1 ) ;
-    source_tmp = source_tmp / dF_norm ;
-
-    Sym_tensor d_gam(map, COV, bspher) ;
-    d_gam = contract( gamma.connect().get_delta(), 0, F.derive_cov(fmets), 0) ; 
-
-    source_tmp -= contract( gamma.con() - s_unit*s_unit, 0, 1,
-			    d_gam, 0, 1) / dF_norm ; 
-    
-    source_tmp = psi4*dF_norm*( source_tmp - k_dd.trace(gamma) + 
-				contract(s_unit, 0, contract(s_unit, 0, k_dd, 1), 0) ) ;
-    
-    source_tmp.std_spectral_base() ;
-
-
-    Valeur sou_angu(g_angu) ; // source defined on the angular grid 
-                              // S(theta, phi) = S(h(theta,phi),theta,phi) 
-    sou_angu.annule_hard() ;
-
-    for (int l=0; l<nz; l++) {
-
-      int jmax = mg->get_nt(l) ;
-      int kmax = mg->get_np(l) ;
-
-      for (int k=0; k<kmax; k++) {
-	for (int j=0; j<jmax; j++) {
-
-	  sou_angu.set(l,k,j,0) = source_tmp.val_point(h(l,k,j,0),(+theta)(l,k,j,0)
-						     ,(+phi)(l,k,j,0))  ;  
-	}
-      }
-    }
-
-    sou_angu = h*h*sou_angu  ;   // Final source term: psi4*dF_norm*h^2*(source_tmp)  
-    sou_angu.std_base_scal() ; 
-
-    // Done with the source term
-    //-----------------------------------------
-
-
-    // Start solving the equation L^2h - 2h = source
-    //-----------------------------------------------
-    
-    sou_angu.ylm() ;
-
-    Valeur h_new = sou_angu ;
-
-    h_new.c_cf->poisson_angu(-2.) ; 
-
-    h_new.ylm_i() ;      
-
-    if (h_new.c != 0x0)
-	delete h_new.c ;
-    h_new.c = 0x0 ;
-    h_new.coef_i() ;
-
-    // Convergence condition:  
-    diff_h = max(abs(h - h_new)) ;
-
-
-    // Relaxations
-    if (step >= step_relax) {
-      h_new = relax * h_new + relax_prev * h ;
-    }
-    
-    // Recycling for the next step
-    h = h_new ;
-
-
-    if (print) 
-      {
-
-	cout << "-------------------------------------" << endl ;
-	cout << "App_hor iteration step: " << step << endl ;
-	cout << "    " << endl ;
-
-	cout << "Difference in h : " << diff_h << endl ;
-
-	// Check: calculate the difference between ex_fcn and ex_fcn_old
-	Tbl diff_exfcn_tbl = diffrel( ex_fcn, ex_fcn_old ) ;
-	diff_exfcn = diff_exfcn_tbl(0) ;
-	for (int l=1; l<nz; l++) {
-	  diff_exfcn += diff_exfcn_tbl(l) ;
-	}
-	diff_exfcn /= nz ;
-	cout << "diff_exfcn : " << diff_exfcn << endl ;
-	
-	ex_fcn_old = ex_fcn ; // recycling 
-	// End check
-    
-      }
-
-    if ( (step == step_max-1) && (max(diff_h) > precis) ) {
-
-
-      //Check: Evaluate the expansion function on the 2-surface
+  for (int step=0 ; 
+       (max(diff_h) > precis) && (step < step_max) && (!no_AH_in_grid);
+       step++) {
       
+      
+      // ***To be fixed: the function "set_grid_point" does not delete the derived 
+      //                 quantities of F.
+      // Temporary fix: Define the level set F inside the iteration loop...
+      //----------------------------------------------------------------
+      
+      Scalar F(map) ;  // level set function: F = r - h(theta,phi) 
+      F.allocate_all() ;
+
       for (int l=0; l<nz; l++) {
-
-	int jmax = mg->get_nt(l) ;
-	int kmax = mg->get_np(l) ;
-
-	for (int k=0; k<kmax; k++) {
-	  for (int j=0; j<jmax; j++) {
-
-	    ex_AH.set(l,k,j,0) = ex_fcn.val_point(h(l,k,j,0),(+theta)(l,k,j,0)
-						     ,(+phi)(l,k,j,0))  ;  
+	  
+	  int imax = mg->get_nr(l) ;
+	  int jmax = mg->get_nt(l) ;
+	  int kmax = mg->get_np(l) ;
+	  
+	  for (int k=0; k<kmax; k++) {
+	      for (int j=0; j<jmax; j++) {
+		  for (int i=0; i<imax; i++) {
+		      
+		      // (+rr) converts rr to Mtbl
+		      F.set_grid_point(l,k,j,i) = (+rr)(l,k,j,i) - h(l,k,j,0) ; 
+		      
+		  }
+	      }
 	  }
-	}
       }
+      
+      F.std_spectral_base() ;
+      
+      // Construct the unit normal vector s^i of the surface F
+      Scalar dF_norm(map) ;   
+      dF_norm = contract( contract(gamma.con(), 0, F.derive_cov(gamma), 0), 
+			0, F.derive_cov(gamma), 0) ;
+      dF_norm = sqrt( dF_norm ) ;
+      dF_norm.std_spectral_base() ;
+      
+      s_unit = F.derive_con(gamma) / dF_norm ; 
+      
+      // The expansion function
+      ex_fcn = s_unit.divergence(gamma) - k_dd.trace(gamma) + 
+      contract( s_unit, 0, contract(s_unit, 0, k_dd, 1), 0) ; 
+      
+      // Construct the source term for the angular Laplace equation
+      //---------------------------------------------------------
 
+      Sym_tensor sou_1(map, CON, bspher) ;
+      sou_1 = gamma.con() - fmets.con()/psi4  - s_unit*s_unit  ;
+      
+      Scalar source_tmp(map) ;
+      source_tmp = contract( sou_1, 0, 1, F.derive_cov(fmets).derive_cov(fmets), 0, 1 ) ;
+      source_tmp = source_tmp / dF_norm ;
+      
+      Sym_tensor d_gam(map, COV, bspher) ;
+      d_gam = contract( gamma.connect().get_delta(), 0, F.derive_cov(fmets), 0) ; 
+      
+      source_tmp -= contract( gamma.con() - s_unit*s_unit, 0, 1,
+			      d_gam, 0, 1) / dF_norm ; 
+    
+      source_tmp = psi4*dF_norm*( source_tmp - k_dd.trace(gamma) + 
+				contract(s_unit, 0, contract(s_unit, 0, k_dd, 1), 0) ) ;
+      
+      source_tmp.std_spectral_base() ;
+      
+      
+      Valeur sou_angu(g_angu) ; // source defined on the angular grid 
+      // S(theta, phi) = S(h(theta,phi),theta,phi) 
+      sou_angu.annule_hard() ;
+      
+      double h_min = min(h)(0) ;
+      double h_max = max(h)(0) ;
+      if ( (r_min < h_min) && (h_max < r_max) ) { 
+	  
+	  for (int l=0; l<nz; l++) {
+	      
+	      int jmax = mg->get_nt(l) ;
+	      int kmax = mg->get_np(l) ;
+	      for (int k=0; k<kmax; k++) {
+		  for (int j=0; j<jmax; j++) {
+		    sou_angu.set(l,k,j,0) = source_tmp.val_point(h(l,k,j,0)
+					  ,(+theta)(l,k,j,0) ,(+phi)(l,k,j,0)) ;  
+		}
+	      }
+	  }
+	  sou_angu = h*h*sou_angu  ; // Final source term: psi4*dF_norm*h^2*(source_tmp)  
+      } 
+      else {
+	  no_AH_in_grid = true ;
+	  break ;
+      }
+      sou_angu.std_base_scal() ; 
+      
+      
+      // Done with the source term
+      //-----------------------------------------
+      
+      
+      // Start solving the equation L^2h - 2h = source
+      //-----------------------------------------------
+      
+      sou_angu.ylm() ;
+      
+      Valeur h_new = sou_angu ;
+      
+      h_new.c_cf->poisson_angu(-2.) ; 
+      
+      h_new.ylm_i() ;      
+      
+      if (h_new.c != 0x0)
+	  delete h_new.c ;
+      h_new.c = 0x0 ;
+      h_new.coef_i() ;
+      
+      // Convergence condition:  
+      diff_h = max(abs(h - h_new)) ;
+      
+      
+      // Relaxations
+      if (step >= step_relax) {
+	  h_new = relax * h_new + relax_prev * h ;
+      }
+      
+      // Recycling for the next step
+      h = h_new ;
 
+      
+      if (print) 
+      {
+	  
+	  cout << "-------------------------------------" << endl ;
+	  cout << "App_hor iteration step: " << step << endl ;
+	  cout << "    " << endl ;
+	  
+	  cout << "Difference in h : " << diff_h << endl ;
+	  
+	  // Check: calculate the difference between ex_fcn and ex_fcn_old
+	  Tbl diff_exfcn_tbl = diffrel( ex_fcn, ex_fcn_old ) ;
+	  diff_exfcn = diff_exfcn_tbl(0) ;
+	  for (int l=1; l<nz; l++) {
+	      diff_exfcn += diff_exfcn_tbl(l) ;
+	  }
+	  diff_exfcn /= nz ;
+	  cout << "diff_exfcn : " << diff_exfcn << endl ;
+	  
+	  ex_fcn_old = ex_fcn ; // recycling 
+	  // End check
+    
+      }
+      
+      if ( (step == step_max-1) && (max(diff_h) > precis) ) {
+	  
+	  
+	  //Check: Evaluate the expansion function on the 2-surface
+	  
+      for (int l=0; l<nz; l++) {
+	  
+	  int jmax = mg->get_nt(l) ;
+	  int kmax = mg->get_np(l) ;
+	  
+	  for (int k=0; k<kmax; k++) {
+	      for (int j=0; j<jmax; j++) {
+		  
+		  ex_AH.set(l,k,j,0) = ex_fcn.val_point(h(l,k,j,0),(+theta)(l,k,j,0)
+							,(+phi)(l,k,j,0))  ;  
+	      }
+	  }
+      }
+      
+      
       cout << " " << endl ;
       cout << "###############################################" << endl ;
       cout << "AH finder: maximum number of iteration reached!" << endl ;
@@ -371,63 +384,74 @@ bool ah_finder(const Metric& gamma, const Sym_tensor& k_dd, Valeur& h, Scalar& e
       cout << "###############################################" << endl ;
       cout << " " << endl ;
 
-    }
-
-
+      }
+      
+      
   } // End of iteration
-
+  
   //Done with the AH finder
+  
 
-
-
+  
   //Check: Evaluate the expansion function on the 2-surface
-
-    for (int l=0; l<nz; l++) {
-
+  
+  if (no_AH_in_grid) {
+      if (print) {
+      cout << " " << endl ;
+      cout << "###############################################" << endl ;
+      cout << " AH finder: no horizon found inside the grid!" << endl ;
+      cout << " Grid: rmin= " << r_min << ", rmax= " << r_max << endl ;
+      cout << "###############################################" << endl ;
+      cout << " " << endl ;
+      }
+  }
+  else {
+  for (int l=0; l<nz; l++) {
+      
       int jmax = mg->get_nt(l) ;
       int kmax = mg->get_np(l) ;
-
+      
       for (int k=0; k<kmax; k++) {
 	for (int j=0; j<jmax; j++) {
-
-	  ex_AH.set(l,k,j,0) = ex_fcn.val_point(h(l,k,j,0),(+theta)(l,k,j,0)
-						     ,(+phi)(l,k,j,0))  ;  
+	    
+	    ex_AH.set(l,k,j,0) = ex_fcn.val_point(h(l,k,j,0),(+theta)(l,k,j,0)
+						  ,(+phi)(l,k,j,0))  ;  
 	}
       }
-    }
-
-
-
+  }
+  
+  
+  
   if ( (max(diff_h) < precis) && (max(abs(ex_AH(0))) < precis_exp) ) {
-
+      
       ah_flag = true ; 
 
-    cout << "  " << endl ;
-    cout << "################################################" << endl ;
-    cout << " AH finder: Apparent horizon found!!!      " << endl ;
-    cout << " Max error of the expansion function on h: " << endl ;
-    cout << " max( expansion function on AH ) = " << max(abs(ex_AH(0))) << endl ; 
-    cout << "################################################" << endl ;
-    cout << " " << endl ;
-
-
+      cout << "  " << endl ;
+      cout << "################################################" << endl ;
+      cout << " AH finder: Apparent horizon found!!!      " << endl ;
+      cout << " Max error of the expansion function on h: " << endl ;
+      cout << " max( expansion function on AH ) = " << max(abs(ex_AH(0))) << endl ; 
+      cout << "################################################" << endl ;
+      cout << " " << endl ;
+      
+      
   }
-
+  
   if ( (max(diff_h) < precis) && (max(abs(ex_AH(0))) > precis_exp) ) {
 
-    if (print) {
-      cout << " " << endl ;
-      cout << "#############################################" << endl ;
-      cout << " AH finder: convergence in the 2 surface h.   " << endl ;
-      cout << " But max error of the expansion function evaulated on h > precis_exp"  << endl ;
-      cout << "   max( expansion function on AH ) =  " << max(abs(ex_AH(0))) << endl ;
-      cout << " Probably not an apparent horizon! " << endl ;
-      cout << "#############################################" << endl ;
-      cout << "   " << endl ;
-    }
+      if (print) {
+	  cout << " " << endl ;
+	  cout << "#############################################" << endl ;
+	  cout << " AH finder: convergence in the 2 surface h.   " << endl ;
+	  cout << " But max error of the expansion function evaulated on h > precis_exp"  << endl ;
+	  cout << "   max( expansion function on AH ) =  " << max(abs(ex_AH(0))) << endl ;
+	  cout << " Probably not an apparent horizon! " << endl ;
+	  cout << "#############################################" << endl ;
+	  cout << "   " << endl ;
+      }
 
   }
-
+  }
   return ah_flag ;
   
 } // End ah_finder
