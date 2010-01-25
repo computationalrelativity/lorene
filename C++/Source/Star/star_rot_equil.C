714ca46b730889fae82d5741c83125199ac056b8
e_gourgoulhon <e_gourgoulhon>
2010-01-25 22:32:38 +0000

    Start of real implementation

--- C++/Source/Star/star_rot_equil.C
+++ C++/Source/Star/star_rot_equil.C
@@ -32,9 +32,11 @@ char star_rot_equil_C[] = "$Header$" ;
 /*
  * $Id$
  * $Log$
+ * Revision 1.2  2010/01/25 22:32:38  e_gourgoulhon
+ * Start of real implementation
+ *
  * Revision 1.1  2010/01/25 18:15:52  e_gourgoulhon
  * First version.
- *
  *
  *
  * $Header$
@@ -47,6 +49,7 @@ char star_rot_equil_C[] = "$Header$" ;
 // Headers Lorene
 #include "star_rot.h"
 #include "param.h"
+#include "cmp.h"
 
 #include "graphique.h"
 #include "utilitaires.h"
@@ -57,7 +60,467 @@ void Star_rot::equilibrium(double ent_c, double omega0, double fact_omega,
 			     const Tbl& control, double mbar_wanted, 
 			     double aexp_mass, Tbl& diff, Param*) {
 			     
-	cout << "Star_rot::equilibrium : not implemented yet ! " << endl ; 
+    // Fundamental constants and units
+    // -------------------------------
+
+    using namespace Unites ;	    
+    
+    // For the display 
+    // ---------------
+    char display_bold[]="x[1m" ; display_bold[0] = 27 ;
+    char display_normal[] = "x[0m" ; display_normal[0] = 27 ;
+
+    // Grid parameters
+    // ---------------
+    
+    const Mg3d* mg = mp.get_mg() ; 
+    int nz = mg->get_nzone() ;	    // total number of domains
+    int nzm1 = nz - 1 ; 
+    
+    // The following is required to initialize mp_prev as a Map_et:
+    Map_et& mp_et = dynamic_cast<Map_et&>(mp) ; 
+        
+    // Index of the point at phi=0, theta=pi/2 at the surface of the star:
+    assert(mg->get_type_t() == SYM) ; 
+    int l_b = nzet - 1 ; 
+    int i_b = mg->get_nr(l_b) - 1 ; 
+    int j_b = mg->get_nt(l_b) - 1 ; 
+    int k_b = 0 ; 
+    
+    // Value of the enthalpy defining the surface of the star
+    double ent_b = ent_limit(nzet-1) ;
+    
+    // Parameters to control the iteration
+    // -----------------------------------
+    
+    int mer_max = icontrol(0) ; 
+    int mer_rot = icontrol(1) ;
+    int mer_change_omega = icontrol(2) ; 
+    int mer_fix_omega = icontrol(3) ; 
+    int mer_mass = icontrol(4) ; 
+    int mermax_poisson = icontrol(5) ; 
+    int mer_triax = icontrol(6) ; 
+    int delta_mer_kep = icontrol(7) ; 
+
+    // Protections:
+    if (mer_change_omega < mer_rot) {
+	cout << "Etoile_rot::equilibrium: mer_change_omega < mer_rot !" << endl ;
+	cout << " mer_change_omega = " << mer_change_omega << endl ; 
+	cout << " mer_rot = " << mer_rot << endl ; 
 	abort() ; 
+    }
+    if (mer_fix_omega < mer_change_omega) {
+	cout << "Etoile_rot::equilibrium: mer_fix_omega < mer_change_omega !" 
+	     << endl ;
+	cout << " mer_fix_omega = " << mer_fix_omega << endl ; 
+	cout << " mer_change_omega = " << mer_change_omega << endl ; 
+	abort() ; 
+    }
+
+    // In order to converge to a given baryon mass, shall the central
+    // enthalpy be varied or Omega ?
+    bool change_ent = true ; 
+    if (mer_mass < 0) {
+	change_ent = false ; 
+	mer_mass = abs(mer_mass) ;
+    }
+
+    double precis = control(0) ; 
+    double omega_ini = control(1) ; 
+    double relax = control(2) ;
+    double relax_prev = double(1) - relax ;  
+    double relax_poisson = control(3) ; 
+    double thres_adapt = control(4) ; 
+    double ampli_triax = control(5) ; 
+    double precis_adapt = control(6) ; 
+
+
+    // Error indicators
+    // ----------------
+    
+    diff.set_etat_qcq() ; 
+    double& diff_ent = diff.set(0) ; 
+    double& diff_nuf = diff.set(1) ; 
+    double& diff_nuq = diff.set(2) ; 
+//    double& diff_dzeta = diff.set(3) ; 
+//    double& diff_ggg = diff.set(4) ; 
+    double& diff_shift_x = diff.set(5) ; 
+    double& diff_shift_y = diff.set(6) ; 
+    double& vit_triax = diff.set(7) ; 
+    
+    // Parameters for the function Map_et::adapt
+    // -----------------------------------------
+    
+    Param par_adapt ; 
+    int nitermax = 100 ;  
+    int niter ; 
+    int adapt_flag = 1 ;    //  1 = performs the full computation, 
+			    //  0 = performs only the rescaling by 
+			    //      the factor alpha_r
+    int nz_search = nzet + 1 ;  // Number of domains for searching the enthalpy
+				//  isosurfaces
+    double alpha_r ; 
+    double reg_map = 1. ; // 1 = regular mapping, 0 = contracting mapping
+
+    par_adapt.add_int(nitermax, 0) ; // maximum number of iterations to
+				     // locate zeros by the secant method
+    par_adapt.add_int(nzadapt, 1) ; // number of domains where the adjustment 
+				    // to the isosurfaces of ent is to be 
+				    // performed
+    par_adapt.add_int(nz_search, 2) ;	// number of domains to search for
+					// the enthalpy isosurface
+    par_adapt.add_int(adapt_flag, 3) ; //  1 = performs the full computation, 
+				       //  0 = performs only the rescaling by 
+				       //      the factor alpha_r
+    par_adapt.add_int(j_b, 4) ; //  theta index of the collocation point 
+			        //  (theta_*, phi_*)
+    par_adapt.add_int(k_b, 5) ; //  theta index of the collocation point 
+			        //  (theta_*, phi_*)
+
+    par_adapt.add_int_mod(niter, 0) ;  //  number of iterations actually used in 
+				       //  the secant method
+    
+    par_adapt.add_double(precis_adapt, 0) ; // required absolute precision in 
+					     // the determination of zeros by 
+					     // the secant method
+    par_adapt.add_double(reg_map, 1)	;  // 1. = regular mapping, 0 = contracting mapping
+    
+    par_adapt.add_double(alpha_r, 2) ;	// factor by which all the radial 
+					   // distances will be multiplied 
+    	   
+    par_adapt.add_tbl(ent_limit, 0) ;	// array of values of the field ent 
+				        // to define the isosurfaces. 			   
+
+    // Parameters for the function Map_et::poisson for nuf
+    // ----------------------------------------------------
+
+    double precis_poisson = 1.e-16 ;     
+
+    Cmp cssjm1_nuf(ssjm1_nuf) ; 
+    Cmp cssjm1_nuq(ssjm1_nuq) ; 
+    Cmp cssjm1_tggg(ssjm1_tggg) ; 
+    Cmp cssjm1_khi(ssjm1_khi) ; 
+
+    Param par_poisson_nuf ; 
+    par_poisson_nuf.add_int(mermax_poisson,  0) ;  // maximum number of iterations
+    par_poisson_nuf.add_double(relax_poisson,  0) ; // relaxation parameter
+    par_poisson_nuf.add_double(precis_poisson, 1) ; // required precision
+    par_poisson_nuf.add_int_mod(niter, 0) ;  //  number of iterations actually used 
+    par_poisson_nuf.add_cmp_mod( cssjm1_nuf ) ; 
+					   
+    Param par_poisson_nuq ; 
+    par_poisson_nuq.add_int(mermax_poisson,  0) ;  // maximum number of iterations
+    par_poisson_nuq.add_double(relax_poisson,  0) ; // relaxation parameter
+    par_poisson_nuq.add_double(precis_poisson, 1) ; // required precision
+    par_poisson_nuq.add_int_mod(niter, 0) ;  //  number of iterations actually used 
+    par_poisson_nuq.add_cmp_mod( cssjm1_nuq ) ; 
+					   
+    Param par_poisson_tggg ; 
+    par_poisson_tggg.add_int(mermax_poisson,  0) ;  // maximum number of iterations
+    par_poisson_tggg.add_double(relax_poisson,  0) ; // relaxation parameter
+    par_poisson_tggg.add_double(precis_poisson, 1) ; // required precision
+    par_poisson_tggg.add_int_mod(niter, 0) ;  //  number of iterations actually used 
+    par_poisson_tggg.add_cmp_mod( cssjm1_tggg ) ; 
+    double lambda_tggg ;
+    par_poisson_tggg.add_double_mod( lambda_tggg ) ; 
+    
+    Param par_poisson_dzeta ; 
+    double lbda_grv2 ;
+    par_poisson_dzeta.add_double_mod( lbda_grv2 ) ; 
+ 					   
+    // Parameters for the function Scalar::poisson_vect
+    // -------------------------------------------------
+
+    Param par_poisson_vect ; 
+
+    par_poisson_vect.add_int(mermax_poisson,  0) ;  // maximum number of iterations
+    par_poisson_vect.add_double(relax_poisson,  0) ; // relaxation parameter
+    par_poisson_vect.add_double(precis_poisson, 1) ; // required precision
+    par_poisson_vect.add_cmp_mod( cssjm1_khi ) ; 
+    par_poisson_vect.add_tensor_mod( ssjm1_wshift ) ; 
+    par_poisson_vect.add_int_mod(niter, 0) ;   
+
+ 					   
+    // Initializations
+    // ---------------
+
+    // Initial angular velocity
+    omega = 0 ; 
+  
+    double accrois_omega = (omega0 - omega_ini) / 
+			    double(mer_fix_omega - mer_change_omega) ; 
+
+
+    update_metric() ;	// update of the metric coefficients
+
+    equation_of_state() ;	// update of the density, pressure, etc...
+    
+    hydro_euler() ;	// update of the hydro quantities relative to the 
+			//  Eulerian observer
+
+    // Quantities at the previous step : 	
+    Map_et mp_prev = mp_et ; 
+    Scalar ent_prev = ent ;	    
+    Scalar logn_prev = logn ;	    
+    Scalar dzeta_prev = dzeta ;	    
+    
+    // Creation of uninitialized tensors:
+    Scalar source_nuf(mp) ;    // source term in the equation for nuf
+    Scalar source_nuq(mp) ;    // source term in the equation for nuq
+    Scalar source_dzf(mp) ;	// matter source term in the eq. for dzeta
+    Scalar source_dzq(mp) ;	// quadratic source term in the eq. for dzeta
+    Scalar source_tggg(mp) ;	// source term in the eq. for tggg
+    Vector source_shift(mp, CON, mp.get_bvect_cart()) ;  
+						    // source term for shift
+    Scalar mlngamma(mp) ;	// centrifugal potential
+    
+		    
+    ofstream fichconv("convergence.d") ;    // Output file for diff_ent
+    fichconv << "#     diff_ent     GRV2       max_triax      vit_triax" << endl ; 
+    
+    ofstream fichfreq("frequency.d") ;    // Output file for  omega
+    fichfreq << "#       f [Hz]" << endl ; 
+    
+    ofstream fichevol("evolution.d") ;    // Output file for various quantities
+    fichevol << 
+    "#       |dH/dr_eq/dH/dr_pole|      r_pole/r_eq	ent_c" 
+    << endl ; 
+    
+    diff_ent = 1 ; 
+    double err_grv2 = 1 ; 
+    double max_triax_prev = 0 ;	    // Triaxial amplitude at previous step
+
+    //=========================================================================
+    // 			Start of iteration
+    //=========================================================================
+
+    for(int mer=0 ; (diff_ent > precis) && (mer<mer_max) ; mer++ ) {
+
+ 	cout << "-----------------------------------------------" << endl ;
+	cout << "step: " << mer << endl ;
+	cout << "diff_ent = " << display_bold << diff_ent << display_normal
+	     << endl ;    
+	cout << "err_grv2 = " << err_grv2 << endl ;    
+	fichconv << mer ;
+	fichfreq << mer ;
+	fichevol << mer ;
+
+	if (mer >= mer_rot) {
+	
+	    if (mer < mer_change_omega) {
+		omega = omega_ini ; 
+	    }
+	    else {
+		if (mer <= mer_fix_omega) {
+		    omega = omega_ini + accrois_omega * 
+			      (mer - mer_change_omega) ;
+		}
+	    }
+
+	}
+
+	//-----------------------------------------------
+	//  Sources of the Poisson equations
+	//-----------------------------------------------
+	
+	// Source for nu
+	// -------------
+	Scalar bet = log(bbb) ; 
+	bet.std_spectral_base() ; 
+
+	Vector d_logn = logn.derive_cov( mp.flat_met_spher() ) ; 
+	Vector d_bet = bet.derive_cov( mp.flat_met_spher() ) ; 
+
+	if (relativistic) {
+	    source_nuf =  qpig * a_car *( ener_euler + s_euler ) ; 
+
+	    source_nuq = ak_car - d_logn(1)*(d_logn(1)+d_bet(1))
+		- d_logn(2)*(d_logn(2)+d_bet(2))
+		- d_logn(3)*(d_logn(3)+d_bet(3)) ; 
+	}
+	else {
+	    source_nuf = qpig * nbar ; 
+
+	    source_nuq = 0 ; 
+	}
+	source_nuf.std_spectral_base() ; 	
+	source_nuq.std_spectral_base() ; 	
+
+	// Source for dzeta
+	// ----------------
+	source_dzf = 2 * qpig * a_car * (press + (ener_euler+press) * uuu*uuu ) ;
+	source_dzf.std_spectral_base() ; 
+  
+	source_dzq = 1.5 * ak_car 
+	    - d_logn(1)*d_logn(1) - d_logn(2)*d_logn(2) - d_logn(3)*d_logn(3) ;	    
+	source_dzq.std_spectral_base() ; 	
+	
+	// Source for tggg
+	// ---------------
+	
+	source_tggg = 4 * qpig * nn * a_car * bbb * press ;
+	source_tggg.std_spectral_base() ; 
+	
+	source_tggg.mult_rsint() ; 
+	
+
+	// Source for shift
+	// ----------------
+	    
+	// Matter term: 
+	source_shift = (-4*qpig) * nn * a_car  * (ener_euler + press)
+				* u_euler ;
+
+	// Quadratic terms:
+	Vector vtmp =  6 * bet.derive_con( mp.flat_met_spher() ) 
+	    - 2 * logn.derive_con( mp.flat_met_spher() ) ;
+	vtmp.change_triad(mp.get_bvect_cart()) ; 
+
+	Vector squad  = nn * contract(tkij, 1, vtmp, 0) ; 
+
+	source_shift = source_shift + squad.up(0, mp.flat_met_cart() ) ; 
+
+	//----------------------------------------------
+	// Resolution of the Poisson equation for nuf 
+	//----------------------------------------------
+
+	source_nuf.poisson(par_poisson_nuf, nuf) ; 
+	
+	cout << "Test of the Poisson equation for nuf :" << endl ; 
+	Tbl err = source_nuf.test_poisson(nuf, cout, true) ; 
+	diff_nuf = err(0, 0) ; 
+
+	//---------------------------------------
+	// Triaxial perturbation of nuf
+	//---------------------------------------
+	
+	if (mer == mer_triax) {
+	
+	    if ( mg->get_np(0) == 1 ) {
+		cout << 
+		"Etoile_rot::equilibrium: np must be stricly greater than 1"
+		<< endl << " to set a triaxial perturbation !" << endl ; 
+		abort() ; 
+	    }
+	
+	    const Coord& phi = mp.phi ; 
+	    const Coord& sint = mp.sint ; 
+	    Scalar perturb(mp) ; 
+	    perturb = 1 + ampli_triax * sint*sint * cos(2*phi) ;
+	    nuf = nuf * perturb ;  
+	    
+	    nuf.std_spectral_base() ;    // set the bases for spectral expansions
+				    //  to be the standard ones for a 
+				    //  scalar field
+
+	}
+	
+	// Monitoring of the triaxial perturbation
+	// ---------------------------------------
+	
+	const Valeur& va_nuf = nuf.get_spectral_va() ; 
+	va_nuf.coef() ;		// Computes the spectral coefficients
+	double max_triax = 0 ; 
+
+	if ( mg->get_np(0) > 1 ) {
+
+	    for (int l=0; l<nz; l++) {	// loop on the domains
+		for (int j=0; j<mg->get_nt(l); j++) {
+		    for (int i=0; i<mg->get_nr(l); i++) {
+		
+			// Coefficient of cos(2 phi) : 
+			double xcos2p = (*(va_nuf.c_cf))(l, 2, j, i) ; 
+
+			// Coefficient of sin(2 phi) : 
+			double xsin2p = (*(va_nuf.c_cf))(l, 3, j, i) ; 
+		    
+			double xx = sqrt( xcos2p*xcos2p + xsin2p*xsin2p ) ; 
+		    
+			max_triax = ( xx > max_triax ) ? xx : max_triax ; 		    
+		    }
+		} 
+	    }
+	    
+	}
+	
+	cout << "Triaxial part of nuf : " << max_triax << endl ; 
+
+	if (relativistic) {
+	    
+	    //----------------------------------------------
+	    // Resolution of the Poisson equation for nuq 
+	    //----------------------------------------------
+
+	    source_nuq.poisson(par_poisson_nuq, nuq) ; 
+	    
+	    cout << "Test of the Poisson equation for nuq :" << endl ; 
+	    err = source_nuq.test_poisson(nuq, cout, true) ;
+	    diff_nuq = err(0, 0) ; 
+	
+	    //---------------------------------------------------------
+	    // Resolution of the vector Poisson equation for the shift
+	    //---------------------------------------------------------
+
+
+	    for (int i=1; i<=3; i++) {
+	      if(source_shift(i).get_etat() != ETATZERO) {
+		    if(source_shift(i).dz_nonzero()) {
+			assert( source_shift(i).get_dzpuis() == 4 ) ; 
+		    }
+		    else{
+			(source_shift.set(i)).set_dzpuis(4) ; 
+		    }
+		}
+	    }
+
+	    
+	    //##
+	    // source_shift.dec2_dzpuis() ;    // dzpuis 4 -> 2
+
+	    double lambda_shift = double(1) / double(3) ; 
+
+	    if ( mg->get_np(0) == 1 ) {
+		lambda_shift = 0 ; 
+	    }
+	
+	    //source_shift.poisson_vect(lambda_shift, par_poisson_vect, 
+	    //			      shift, w_shift, khi_shift) ;      
+	    
+	    cout << "Test of the Poisson equation for shift_x :" << endl ; 
+	    err = source_shift(1).test_poisson(-beta(1), cout, true) ;
+	    diff_shift_x = err(0, 0) ; 
+	
+	    cout << "Test of the Poisson equation for shift_y :" << endl ; 
+	    err = source_shift(2).test_poisson(-beta(2), cout, true) ;
+	    diff_shift_y = err(0, 0) ; 
+	    
+	    // Computation of tnphi and nphi from the Cartesian components
+	    //  of the shift
+	    // -----------------------------------------------------------
+	    
+	    fait_nphi() ; 
+	
+	}
+
+
+	
+    } // End of main loop
+    
+    //=========================================================================
+    // 			End of iteration
+    //=========================================================================
+
+    fichconv.close() ; 
+    fichfreq.close() ; 
+    fichevol.close() ; 
+    
+    ssjm1_nuf = cssjm1_nuf ; 
+    ssjm1_nuq = cssjm1_nuq ; 
+    ssjm1_tggg = cssjm1_tggg ; 
+    ssjm1_khi = cssjm1_khi ; 
+
+	cout << "Star_rot::equilibrium : not finished ! " << endl ; 
+	abort() ; 
+
 
 }
