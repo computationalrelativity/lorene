15efd3b5c13f6f80f7f4586b41a16de2205b32c0
n_vasset <n_vasset>
2010-01-29 14:38:59 +0000

    New members expa and dt_expa. Several new routines for evolution of boundary conditions.

--- C++/Include/excision_surf.h
+++ C++/Include/excision_surf.h
@@ -62,13 +62,18 @@ class Excision_surf {
   
   /** The 3-d extrinsic curvature on the slice */
   Sym_tensor Kij ;
-
+  
   /** The time step for evolution in parabolic drivers */
   double delta_t;
 
   /** The internal number of timesteps for one iteration. */
   double no_of_steps;
 
+  /** The 2d expansion, directly evolved from the initial excision with Einstein Equations */
+  Scalar expa;
+  
+  /** The time derivative of the expansion, derived from Einstein equations and arbitrary evolution */
+  Scalar dt_expa;
   
 
     // Derived data : 
@@ -82,11 +87,10 @@ class Excision_surf {
 	mutable Scalar* p_get_BC_conf_fact_2 ; ///< Source of Neumann boundary condition on \f$ \psi \f$,  
 	mutable Scalar* p_get_BC_conf_fact_3 ; ///< Source of Neumann boundary condition on \f$ \psi \f$, 
 	mutable Scalar* p_get_BC_conf_fact_4 ; ///< Source of Birichlet boundary condition on \f$ \psi \f$, 
-	mutable Scalar* p_get_BC_expa ; ///< gives a new value for expansion (rescaled with lapse) obtained by parabolic evolution.
 	mutable Scalar* p_get_BC_lapse_2 ;   ///< Source of Dirichlet boundary condition of \f$ N \f$
 	mutable Scalar* p_get_BC_lapse_3 ;   ///< Source of Dirichlet condtion on  \f$ N \f$, based on einstein equations.
 	mutable Scalar* p_get_BC_lapse_4 ;   ///< Source of Dirichlet condtion on  \f$ N \f$, based on einstein equations (conservation of isotropic gauge)
-	mutable Scalar* p_new_expa_from_ID ;   ///< Computation of an updated expansion scalar.
+	mutable Scalar* p_derive_t_expa ;   ///< Computation of an updated expansion scalar.
 	mutable Vector* p_get_BC_shift_2 ; ///< Source of Dirichlet BC for the shift vector  \f$ \beta^{i} \f$
 	mutable Vector* p_get_BC_shift_3 ; ///< Source of Dirichlet BC for the shift vector  \f$ \beta^{i} \f$, partly derived from kinematical relation
 	mutable Vector* p_get_BC_shift_4 ; ///< Source of Dirichlet BC for the shift vector  \f$ \beta^{i} \f$, partly from projection of Einstein Equations
@@ -130,12 +134,30 @@ class Excision_surf {
 	void set_der_0x0() const ; 
 
 
+
     // Mutators / assignment
     // ---------------------
     public:
 	/// Assignment to another Excision_surf
 	void operator=(const Excision_surf&) ;	
-	
+
+
+	// Functions/Accessors
+	//---------------------
+
+	/** Computes the parameters for the hyperbolic evolution in set_expa_hyperb(), so that the expansion has a C1 matching 
+	 * with initial data. Sets also values for expa() and dt_expa() accordingly with initial conditions */
+	void get_evol_params_from_ID(double alpha, double beta, double gamma, Scalar& Ee, Vector& Jj, Sym_tensor& Ss) ;
+
+	///Sets a new value for expansion rescaled over lapse (and its derivative), obtained by parabolic evolution.
+	void set_expa_parab(double c_theta_lap, double c_theta_fin, Scalar& expa_fin) ;
+	/**Sets a new value for expansion rescaled over lapse (and its derivative), obtained by hyperbolic evolution.
+	 * Parameters for the hyperbolic driver are determined by the function Excision_surf::get_evol_params_from_ID() 
+	 * so that the expansion stays of regularity $C^{1}$ throughout.
+	 * All manipulated quantities are 2-dimensional.
+	 */
+	void set_expa_hyperb(double alph0, double beta0, double gamma0) ;
+
 
     // Accessors
     // ---------
@@ -164,6 +186,12 @@ class Excision_surf {
 	/// Returns the internal number of timesteps for one iteration.
 	double get_no_of_steps() const {return no_of_steps ;};
 
+	/// Returns the assumed expansion associated to the excised surface at t.
+	const Scalar & get_expa() const {return expa; };
+
+	/// Returns the assumed time derivative of the expansion at t, evolved by functions of this class; 
+	const Scalar& get_dt_expa() const {return dt_expa;} ;
+
 	/// Sets a new spheroid from data
 	Spheroid& set_sph() {del_deriv() ; return sph ;};
 
@@ -186,14 +214,19 @@ class Excision_surf {
 
 	double set_no_of_steps() {del_deriv() ; return no_of_steps ; } ;
 
+	/// Sets the expansion function on the surface at time t (considering to protect this function)
+
+	Scalar& set_expa() {del_deriv(); return expa;};
+
+	/// Sets the time derivative of the expansion function on the surface at time t (considering to protect this function)
+
+	Scalar& set_dt_expa() {del_deriv(); return dt_expa;};
 	
     // Computational functions
     // -----------------------
     public:
-
-
 	/// Source for a Neumann BC on the conformal factor. If boolean isMOTS is false, it is based on expansion value of the spheroid or the value of exppa; it is based on zero expansion if isMOTS is true. 
-	const Scalar& get_BC_conf_fact_1(bool isMOTS = false, Scalar* exppa = 0x0) const ;
+	const Scalar& get_BC_conf_fact_1(bool isMOTS = false) const ;
 // Source for an arbitrary Dirichlet BC on the lapse	
 	const Scalar& get_BC_lapse_1(double value) const ;
 // Source for a global Dirichlet BC on the shift, imposing a conformal Killing symmetry on \f$ \varphi \f$.
@@ -206,32 +239,27 @@ class Excision_surf {
 	const Scalar& get_BC_conf_fact_3(double c_theta_lap, double c_theta_fin, Scalar& expa_fin) const ;
 /// Source for the Dirchlet BC on the conformal factor, based on the consistency condition derived from the trace
 	const Scalar& get_BC_conf_fact_4() const ;
-	///Gets a new value for expansion rescaled over lapse, obtained by parabolic evolution.
-	const Scalar& get_BC_expa(double c_theta_lap, double c_theta_fin, Scalar& expa_fin) const ;
 /// Source for Dirichlet BC on the lapse, based on a parabolic driver towards arbitrary constant value
 	const Scalar& get_BC_lapse_2(double lapse_fin, double c_lapse_lap, double c_lapse_fi) const ;
 /// Source for Dirichlet BC on the lapse, based on einstein equations
 	const Scalar& get_BC_lapse_3(Scalar& dttheta, Scalar& Ee, Vector& Jj, Sym_tensor& Sij, bool sph_sym = true) const ;
-/// Source for Dirichlet BC on the lapse, based on einstein equations (conservation of isotropic gauge)
+	/// Source for Dirichlet BC on the lapse, based on einstein equations (conservation of isotropic gauge)
 	const Scalar& get_BC_lapse_4 (Scalar& old_nn, Vector& beta_point, Sym_tensor& strain_tens) const ;
-	/// Forms new value  (at the next timestep) for the expansion from Initial data and Einstein equations
-	const Scalar& new_expa_from_ID(Scalar& Ee, Vector& Jj, Sym_tensor& Sij) const;
-/// Source for a Dirichlet BC on the shift, based on a Parabolic driver; no assumptions are made except a global conformal Killing symmetry.
+	/// Forms the prospective time derivative for the expansion using projected Einstein equations. Does NOT modify the member dt_expa: do it by hand!
+	const Scalar& derive_t_expa(Scalar& Ee, Vector& Jj, Sym_tensor& Sij) const;
+	/// Source for a Dirichlet BC on the shift, based on a Parabolic driver; no assumptions are made except a global conformal Killing symmetry.
 	const Vector& get_BC_shift_2(double c_bb_lap, double c_bb_fin, double c_V_lap, double epsilon) const ;
-/// Source for a Dirichlet BC on the shift, based on a Parabolic driver; Radial part is dealt with using a kinematical relation.
+	/// Source for a Dirichlet BC on the shift, based on a Parabolic driver; Radial part is dealt with using a kinematical relation.
 	const Vector& get_BC_shift_3(Scalar& dtpsi, double c_V_lap, double epsilon) const ;
-/// Source for a Dirichlet BC on the shift, based on a Parabolic driver; Radial part is dealt with using projection of Einstein Equations.
+	/// Source for a Dirichlet BC on the shift, based on a Parabolic driver; Radial part is dealt with using projection of Einstein Equations.
 	const Vector & get_BC_shift_4(Scalar& dttheta, Scalar& Ee, Vector& Jj, Sym_tensor& Sij, double c_V_lap, double epsilon, bool sph_sym= true) const ;
-/// Source for the Dirichlet BC on (N*Psi1), based on a parabolic driver.
+	/// Source for the Dirichlet BC on (N*Psi1), based on a parabolic driver.
 	const Scalar& get_BC_Npsi_2(double value, double c_npsi_lap, double c_npsi_fin) const ;        
-
-/// Source for the Dirichlet BC on (N*Psi1), with Kerr_Schild-like form for the lapse boundary.
+	/// Source for the Dirichlet BC on (N*Psi1), with Kerr_Schild-like form for the lapse boundary.
 	const Scalar& get_BC_Npsi_3(double n_0, double beta) const ;      
-
-/// Source for a Dirichlet BC on (N*Psi1), fixing a constant surface gravity in space and time.
+	/// Source for a Dirichlet BC on (N*Psi1), fixing a constant surface gravity in space and time.
 	const Scalar& get_BC_Npsi_4(double Kappa) const ;      
-
-/// Source for a Neumann BC on (N*Psi1), fixing a constant surface gravity in space and time.
+	/// Source for a Neumann BC on (N*Psi1), fixing a constant surface gravity in space and time.
 	const Scalar& get_BC_Npsi_5(double Kappa) const ; 
     // Outputs
     // -------
