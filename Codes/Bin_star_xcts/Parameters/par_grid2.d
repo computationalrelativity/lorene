86a7cad40644d389af611c9f22c89e4c00030e02
m_bejger <m_bejger>
2010-06-23 20:48:39 +0000

    More suitable parameters in par_eos?.d, par_grid?.d

--- Codes/Bin_star_xcts/Parameters/par_grid2.d
+++ Codes/Bin_star_xcts/Parameters/par_grid2.d
@@ -2,12 +2,11 @@
 #######################
 4	nz: total number of domains
 1	nzet: number of domains inside the star
-7	nt: number of points in theta (the same in each domain)
-4	np: number of points in phi   (the same in each domain)
+13	nt: number of points in theta (the same in each domain)
+8	np: number of points in phi   (the same in each domain)
 # Number of points in r and (initial) inner boundary of each domain:
-9	0.	<-   nr	  &   min(r)  in domain 0  (nucleus)
-9	1.	<-   nr	  &   min(r)  in domain 1
-9	1.5	<-   nr   &   min(r)  in domain 2
-9	2.5	<-   nr   &   min(r)  in domain 2
-
+17	0.	<-   nr	  &   min(r)  in domain 0  (nucleus)
+17	1.	<-   nr	  &   min(r)  in domain 1
+17	4.	<-   nr   &   min(r)  in domain 2
+17	8.	<-   nr   &   min(r)  in domain 3 
 
