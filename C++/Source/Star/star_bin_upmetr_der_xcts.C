0f1dbad4bdfdedb9de01efe18c12b4cdf3ab635f
m_bejger <m_bejger>
2010-12-09 10:44:42 +0000

    Cosmetic changes

--- C++/Source/Star/star_bin_upmetr_der_xcts.C
+++ C++/Source/Star/star_bin_upmetr_der_xcts.C
@@ -28,6 +28,9 @@ char star_bin_upmetr_der_xcts_C[] = "$Header$" ;
 /*
  * $Id$
  * $Log$
+ * Revision 1.4  2010/12/09 10:44:42  m_bejger
+ * Cosmetic changes
+ *
  * Revision 1.3  2010/06/15 14:58:19  m_bejger
  * Companion D^j beta^i computed in companion frame and imported
  *
@@ -50,47 +53,42 @@ char star_bin_upmetr_der_xcts_C[] = "$Header$" ;
 #include "graphique.h"
 
 void Star_bin_xcts::update_metric_der_comp(const Star_bin_xcts& comp) {
-  
+
   // Derivatives of metric coefficients
   // ----------------------------------
-      
-    // dcov_Psi
-    
-    Vector temp = (comp.Psi_auto).derive_cov(comp.flat) ;
-    temp.dec_dzpuis(2) ;
-    
-    temp.change_triad(temp.get_mp().get_bvect_cart()) ;
-    temp.change_triad(mp.get_bvect_cart()) ;
-    Base_val sauve_base1 (temp(1).get_spectral_va().get_base()) ;
-    Base_val sauve_base2 (temp(2).get_spectral_va().get_base()) ;
-    Base_val sauve_base3 (temp(3).get_spectral_va().get_base()) ;
-    assert ( *(temp.get_triad()) == *(dcov_Psi.get_triad())) ;
-	
-	for(int i=1; i<=3; i++) dcov_Psi.set(i).import(temp(i)) ;
 
-    dcov_Psi.set(1).set_spectral_va().set_base(sauve_base1) ;
-    dcov_Psi.set(2).set_spectral_va().set_base(sauve_base2) ;
-    dcov_Psi.set(3).set_spectral_va().set_base(sauve_base3) ;
+    // Computation of dcov_Psi
+    Vector temp = (comp.Psi_auto).derive_cov(comp.flat) ;
+
+    temp.dec_dzpuis(2) ;
+    temp.change_triad(mp.get_bvect_cart()) ;
+
+    assert ( *(temp.get_triad()) == *(dcov_Psi.get_triad())) ;
+
+	for(int i=1; i<=3; i++) {
+
+		dcov_Psi.set(i).import(temp(i)) ;
+        dcov_Psi.set(i).set_spectral_va().set_base(temp(i).get_spectral_va().get_base()) ;
+
+	}
     dcov_Psi.inc_dzpuis(2) ;
 
     dcov_Psi += Psi_auto.derive_cov(flat) ;
-    
-    // dcov_chi
-    temp = ((comp.chi_auto).derive_cov(comp.flat)) ;
+
+    // Computation of dcov_chi
+    temp = (comp.chi_auto).derive_cov(comp.flat) ;
+
     temp.dec_dzpuis(2) ;
-    
-    temp.change_triad(temp.get_mp().get_bvect_cart()) ;
     temp.change_triad(mp.get_bvect_cart()) ;
-    sauve_base1 = (temp(1).get_spectral_va().get_base()) ;
-    sauve_base2 = (temp(2).get_spectral_va().get_base()) ;
-    sauve_base3 = (temp(3).get_spectral_va().get_base()) ;
+
     assert ( *(temp.get_triad()) == *(dcov_chi.get_triad())) ;
 
-    for(int i=1; i<=3; i++) dcov_chi.set(i).import(temp(i)) ;
+    for(int i=1; i<=3; i++) {
 
-    dcov_chi.set(1).set_spectral_va().set_base(sauve_base1) ;
-    dcov_chi.set(2).set_spectral_va().set_base(sauve_base2) ;
-    dcov_chi.set(3).set_spectral_va().set_base(sauve_base3) ;
+		dcov_chi.set(i).import(temp(i)) ;
+        dcov_chi.set(i).set_spectral_va().set_base(temp(i).get_spectral_va().get_base()) ;
+
+    }
     dcov_chi.inc_dzpuis(2) ;
 
     dcov_chi += chi_auto.derive_cov(flat) ;
@@ -98,54 +96,56 @@ void Star_bin_xcts::update_metric_der_comp(const Star_bin_xcts& comp) {
   // Computation of \hat{A}^{ij}_{comp}
   // ----------------------------------
 
-  // D^j beta^i    
+  // D^j beta^i
 
   Tensor temp_beta = (comp.beta_auto).derive_con(comp.flat) ;
-  temp_beta.dec_dzpuis(2) ; 
+  temp_beta.dec_dzpuis(2) ;
   temp_beta.change_triad(mp.get_bvect_cart()) ;
 
-  Tensor dbeta_comp(mp, 2, CON, mp.get_bvect_cart()) ; 
-        
+  Tensor dbeta_comp(mp, 2, CON, mp.get_bvect_cart()) ;
+
   assert ( *(temp_beta.get_triad()) == *(dbeta_comp.get_triad()) ) ;
-    
-    for(int i=1; i<=3; i++) 
+
+    for(int i=1; i<=3; i++)
 	for(int j=1; j<=3; j++) {
-    
+
         // importing
 	    (dbeta_comp.set(i,j)).import( (temp_beta)(i,j) );
-		// setting the appropriate basis 
-		(dbeta_comp.set(i,j)).set_spectral_va().set_base(temp_beta(i,j).get_spectral_va().get_base()) ; 
-	             
-	}	 
-   
-   dbeta_comp.inc_dzpuis(2) ; 
-                              
+		// setting appropriate bases
+		(dbeta_comp.set(i,j)).set_spectral_va().set_base(temp_beta(i,j).get_spectral_va().get_base()) ;
+
+	}
+
+   dbeta_comp.inc_dzpuis(2) ;
+
   // Trace of D_j beta^i  :
   Scalar divbeta_comp = beta_comp.divergence(flat) ;
-          	   		    		  
-  for (int i=1; i<=3; i++) 
-    for (int j=i; j<=3; j++) {
-        
-      haij_comp.set(i, j) = dbeta_comp(i, j) + dbeta_comp(j, i) 
-	  - double(2) /double(3) * divbeta_comp * (flat.con())(i,j) ; 
-  
+
+  for (int i=1; i<=3; i++)
+     for (int j=1; j<=i; j++) {
+//##
+//    for (int j=i; j<=3; j++) {
+
+      haij_comp.set(i, j) = dbeta_comp(i, j) + dbeta_comp(j, i)
+	  - double(2) /double(3) * divbeta_comp * (flat.con())(i,j) ;
+
     }
-       
+
   // Computation of (\hat{A}_{ij}\hat{A}^{ij})_{comp}
   // ------------------------------------------------
-  
+
   Sym_tensor haij_auto_cov = haij_auto.up_down(flat) ;
-  
-  haij_comp = 0.5 * pow(Psi, 7.) / chi * haij_comp ;  
+
+  haij_comp = 0.5 * pow(Psi, 7.) / chi * haij_comp ;
   //## for comparison: old formulation
-  //haij_comp = 0.5 * haij_comp / nn ; 
-  haij_comp.std_spectral_base() ; 
-   
-  hacar_comp = contract(haij_auto_cov, 0, 1, haij_comp, 0, 1, true) ; 
-  
+  //haij_comp = 0.5 * haij_comp / nn ;
+  haij_comp.std_spectral_base() ;
+
+  hacar_comp = contract(haij_auto_cov, 0, 1, haij_comp, 0, 1, true) ;
+
   // The derived quantities are obsolete
   // -----------------------------------
-  
+
   del_deriv() ;
-  
-}      
+
+}
