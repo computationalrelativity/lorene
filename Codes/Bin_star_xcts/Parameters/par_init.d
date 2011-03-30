555461613f029455464da21201ea13faf8e6f1e7
m_bejger <m_bejger>
2011-03-30 13:23:23 +0000

    Modified to look as the parameter file for Bin_star/init_bin.C

--- Codes/Bin_star_xcts/Parameters/par_init.d
+++ Codes/Bin_star_xcts/Parameters/par_init.d
@@ -1,5 +1,6 @@
 # Physical parameters for the binary initial conditions
 #######################################################
+1	<- 1 for a relativistic computation
 100.	<- coordinate distance between the two stellar centers [km]
 0.169125   <- initial central enthalpy of star 1
 1	<- rotational state of star 1 : 1 = irrotational, 0 = corotating
