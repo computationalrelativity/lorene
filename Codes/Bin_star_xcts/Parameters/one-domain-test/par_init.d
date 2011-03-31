92c0baa2f4e6c4c7f8c4624d1f9668f7866cb047
m_bejger <m_bejger>
2011-03-31 13:02:47 +0000

    Remove obsolete files

--- Codes/Bin_star_xcts/Parameters/one-domain-test/par_init.d
+++
@@ -1,8 +0,0 @@
-# Physical parameters for the binary initial conditions
-#######################################################
-50.	<- coordinate distance between the two stellar centers [km]
-0.25   <- initial central enthalpy of star 1
-1	<- rotational state of star 1 : 1 = irrotational, 0 = corotating
-0.25   <- initial central enthalpy of star 2
-1	<- rotational state of star 2 : 1 = irrotational, 0 = corotating
-
