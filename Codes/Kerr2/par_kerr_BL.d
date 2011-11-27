0c34a8c38680d190fdb23e129d29f32f0f5c07cf
e_gourgoulhon <e_gourgoulhon>
2011-11-27 14:51:32 +0000

    graphic_out set to 1.

--- Codes/Kerr2/par_kerr_BL.d
+++ Codes/Kerr2/par_kerr_BL.d
@@ -1,5 +1,5 @@
 0.5	Kerr parameter a/M
-0	graphical outputs: 0 = no, 1 = yes
+1	graphical outputs: 0 = no, 1 = yes
 17	nr: number of points in r (the same in each domain)
 9	nt: number of points in theta (the same in each domain)
 1	np: number of points in phi (the same in each domain)
