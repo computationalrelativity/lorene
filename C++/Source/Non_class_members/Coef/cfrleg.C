3fe75da64d5cbef8dc7e7a36f87bfcbda8a961f4
j_novak <j_novak>
2013-06-13 14:17:47 +0000

    Implementation of Legendre inverse coefficient transform.

--- C++/Source/Non_class_members/Coef/cfrleg.C
+++ C++/Source/Non_class_members/Coef/cfrleg.C
@@ -26,6 +26,9 @@ char cfrleg_C[] = "$Header$" ;
 /*
  * $Id$
  * $Log$
+ * Revision 1.4  2013/06/13 14:17:47  j_novak
+ * Implementation of Legendre inverse coefficient transform.
+ *
  * Revision 1.3  2013/06/07 14:44:34  j_novak
  * Coefficient computation for even Legendre basis.
  *
@@ -144,101 +147,195 @@ void cfrleg(const int* deg, const int* dimf, double* ff, const int* dimc,
 }
 
 void cfrlegp(const int* deg, const int* dimf, double* ff, const int* dimc,
-		double* cf)
-
+	     double* cf)
+  
 {
-
-// Dimensions des tableaux ff et cf  :
-    int n1f = dimf[0] ;
-    int n2f = dimf[1] ;
-    int n3f = dimf[2] ;
-    int n1c = dimc[0] ;
-    int n2c = dimc[1] ;
-    int n3c = dimc[2] ;
-
-// Nombres de degres de liberte en r :    
-    int nr = deg[2] ;
+  
+  // Dimensions des tableaux ff et cf  :
+  int n1f = dimf[0] ;
+  int n2f = dimf[1] ;
+  int n3f = dimf[2] ;
+  int n1c = dimc[0] ;
+  int n2c = dimc[1] ;
+  int n3c = dimc[2] ;
+  
+  // Nombres de degres de liberte en r :    
+  int nr = deg[2] ;
+  
+  // Tests de dimension:
+  if (nr > n3f) {
+    cout << "cfrlegp: nr > n3f : nr = " << nr << " ,  n3f = " 
+	 << n3f << endl ;
+    abort () ;
+    exit(-1) ;
+  }
+  if (nr > n3c) {
+    cout << "cfrlegp: nr > n3c : nr = " << nr << " ,  n3c = " 
+	 << n3c << endl ;
+    abort () ;
+    exit(-1) ;
+  }
+  if (n1f > n1c) {
+    cout << "cfrlegp: n1f > n1c : n1f = " << n1f << " ,  n1c = " 
+	 << n1c << endl ;
+    abort () ;
+    exit(-1) ;
+  }
+  if (n2f > n2c) {
+    cout << "cfrlegp: n2f > n2c : n2f = " << n2f << " ,  n2c = " 
+	 << n2c << endl ;
+    abort () ;
+    exit(-1) ;
+  }
+  
+  // Nombre de points:
+  int nm1 = nr - 1;
+  int dnm1 = 2*nr - 1 ;
+  
+  Tbl* Pni = 0x0 ;
+  Tbl* wn = 0x0 ;
+  get_legendre_data(dnm1, Pni, wn) ;
+  double* cf_tmp = new double[nr] ;
+  
+  // boucle sur phi et theta
+  
+  int n2n3f = n2f * n3f ;
+  int n2n3c = n2c * n3c ;
+  
+  /*   
+   * Borne de la boucle sur phi: 
+   *    si n1f = 1, on effectue la boucle une fois seulement.
+   *    si n1f > 1, on va jusqu'a j = n1f-2 en sautant j = 1 (les coefficients
+   *	j=n1f-1 et j=0 ne sont pas consideres car nuls). 
+   */
+  int borne_phi = ( n1f > 1 ) ? n1f-1 : 1 ;
+  
+  for (int j=0; j< borne_phi; j++) {
     
-// Tests de dimension:
-    if (nr > n3f) {
-	cout << "cfrlegp: nr > n3f : nr = " << nr << " ,  n3f = " 
-	<< n3f << endl ;
-	abort () ;
-	exit(-1) ;
-    }
-    if (nr > n3c) {
-	cout << "cfrlegp: nr > n3c : nr = " << nr << " ,  n3c = " 
-	<< n3c << endl ;
-	abort () ;
-	exit(-1) ;
-    }
-    if (n1f > n1c) {
-	cout << "cfrlegp: n1f > n1c : n1f = " << n1f << " ,  n1c = " 
-	<< n1c << endl ;
-	abort () ;
-	exit(-1) ;
-    }
-    if (n2f > n2c) {
-	cout << "cfrlegp: n2f > n2c : n2f = " << n2f << " ,  n2c = " 
-	<< n2c << endl ;
-	abort () ;
-	exit(-1) ;
-    }
-
-// Nombre de points:
-    int nm1 = nr - 1;
-    int dnm1 = 2*nr - 1 ;
-
-    Tbl* Pni = 0x0 ;
-    Tbl* wn = 0x0 ;
-    get_legendre_data(dnm1, Pni, wn) ;
-    double* cf_tmp = new double[nr] ;
-	
- // boucle sur phi et theta
-
-    int n2n3f = n2f * n3f ;
-    int n2n3c = n2c * n3c ;
-
-/*   
- * Borne de la boucle sur phi: 
- *    si n1f = 1, on effectue la boucle une fois seulement.
- *    si n1f > 1, on va jusqu'a j = n1f-2 en sautant j = 1 (les coefficients
- *	j=n1f-1 et j=0 ne sont pas consideres car nuls). 
- */
-    int borne_phi = ( n1f > 1 ) ? n1f-1 : 1 ;
-
-    for (int j=0; j< borne_phi; j++) {
+    if (j==1) continue ;	// on ne traite pas le terme en sin(0 phi)
     
-	if (j==1) continue ;	// on ne traite pas le terme en sin(0 phi)
+    for (int k=0; k<n2f; k++) {
+      
+      int i0 = n2n3f * j + n3f * k ; // indice de depart 
+      double* ff0 = ff + i0 ;    // tableau des donnees a transformer
+      
+      i0 = n2n3c * j + n3c * k ; // indice de depart 
+      double* cf0 = cf + i0 ;    // tableau resultat
+      
+      for (int ii=0; ii<nr; ii++) {
+	cf_tmp[ii] = 0.5*ff0[0]*(*Pni)(2*ii, nm1) 
+	  * (*wn)(nm1) ;
+	for (int jj=1; jj<nr; jj++) {
+	  cf_tmp[ii] += ff0[jj]* (*wn)(nm1+jj) * (*Pni)(2*ii, nm1+jj) ;
+	}
+	cf_tmp[ii] *= double(4*ii+1) ;
+      }
+      cf_tmp[nm1] /= double(4*nm1+1) / double(2*nm1) ;
+      for (int i=0; i<nr; i++)
+	cf0[i] = cf_tmp[i] ;
+      
+    } 	// fin de la boucle sur theta 
+  }	// fin de la boucle sur phi
 
-	for (int k=0; k<n2f; k++) {
-
-	    int i0 = n2n3f * j + n3f * k ; // indice de depart 
-	    double* ff0 = ff + i0 ;    // tableau des donnees a transformer
-
-	    i0 = n2n3c * j + n3c * k ; // indice de depart 
-	    double* cf0 = cf + i0 ;    // tableau resultat
-
-	    for (int ii=0; ii<nr; ii++) {
-	      cf_tmp[ii] = 0.5*ff0[0]*(*Pni)(2*ii, nm1) 
-		* (*wn)(nm1) ;
-	      for (int jj=1; jj<nr; jj++) {
-		cf_tmp[ii] += ff0[jj]* (*wn)(nm1+jj) * (*Pni)(2*ii, nm1+jj) ;
-	      }
-	      cf_tmp[ii] *= double(4*ii+1) ;
-	    }
-	    cf_tmp[nm1] /= double(4*nm1+1) / double(2*nm1) ;
-	    for (int i=0; i<nr; i++)
-	      cf0[i] = cf_tmp[i] ;
-
-	} 	// fin de la boucle sur theta 
-   }	// fin de la boucle sur phi
-
-
+  delete [] cf_tmp ;
+  
 }
 
-void cfrlegi(const int*, const int*, double*, const int*, double*)
 
+void cfrlegi(const int* deg, const int* dimf, double* ff, const int* dimc,
+	     double* cf)
+  
 {
-  c_est_pas_fait("cfrlegi") ;
+  
+  // Dimensions des tableaux ff et cf  :
+  int n1f = dimf[0] ;
+  int n2f = dimf[1] ;
+  int n3f = dimf[2] ;
+  int n1c = dimc[0] ;
+  int n2c = dimc[1] ;
+  int n3c = dimc[2] ;
+  
+  // Nombres de degres de liberte en r :    
+  int nr = deg[2] ;
+  
+  // Tests de dimension:
+  if (nr > n3f) {
+    cout << "cfrlegi: nr > n3f : nr = " << nr << " ,  n3f = " 
+	 << n3f << endl ;
+    abort () ;
+    exit(-1) ;
+  }
+  if (nr > n3c) {
+    cout << "cfrlegi: nr > n3c : nr = " << nr << " ,  n3c = " 
+	 << n3c << endl ;
+    abort () ;
+    exit(-1) ;
+  }
+  if (n1f > n1c) {
+    cout << "cfrlegi: n1f > n1c : n1f = " << n1f << " ,  n1c = " 
+	 << n1c << endl ;
+    abort () ;
+    exit(-1) ;
+  }
+  if (n2f > n2c) {
+    cout << "cfrlegi: n2f > n2c : n2f = " << n2f << " ,  n2c = " 
+	 << n2c << endl ;
+    abort () ;
+    exit(-1) ;
+  }
+  
+  // Nombre de points:
+  int nm1 = nr - 1;
+  int dnm1 = 2*nr - 1 ;
+  
+  Tbl* Pni = 0x0 ;
+  Tbl* wn = 0x0 ;
+  get_legendre_data(dnm1, Pni, wn) ;
+  double* cf_tmp = new double[nr] ;
+  double* gam_tmp = new double[nr] ;
+  
+  // boucle sur phi et theta
+  
+  int n2n3f = n2f * n3f ;
+  int n2n3c = n2c * n3c ;
+  
+  /*   
+   * Borne de la boucle sur phi: 
+   *    si n1f = 1, on effectue la boucle une fois seulement.
+   *    si n1f > 1, on va jusqu'a j = n1f-2 en sautant j = 1 (les coefficients
+   *	j=n1f-1 et j=0 ne sont pas consideres car nuls). 
+   */
+  int borne_phi = ( n1f > 1 ) ? n1f-1 : 1 ;
+  
+  for (int j=0; j< borne_phi; j++) {
+    
+    if (j==1) continue ;	// on ne traite pas le terme en sin(0 phi)
+    
+    for (int k=0; k<n2f; k++) {
+      
+      int i0 = n2n3f * j + n3f * k ; // indice de depart 
+      double* ff0 = ff + i0 ;    // tableau des donnees a transformer
+      
+      i0 = n2n3c * j + n3c * k ; // indice de depart 
+      double* cf0 = cf + i0 ;    // tableau resultat
+      
+      for (int ii=0; ii<nr-1; ii++) {
+	cf_tmp[ii] = 0.5*ff0[0]*(*Pni)(2*ii+1, nm1) 
+	  * (*wn)(nm1) ;
+	gam_tmp[ii] = 0. ;
+	for (int jj=1; jj<nr; jj++) {
+	  cf_tmp[ii] += ff0[jj]* (*wn)(nm1+jj) * (*Pni)(2*ii+1, nm1+jj) ;
+	  gam_tmp[ii] += (*Pni)(2*ii+1, nm1+jj) * (*Pni)(2*ii+1, nm1+jj) * (*wn)(nm1+jj) ;
+	}
+	cf_tmp[ii] *= double(4*ii+3) ;
+      }
+      cf_tmp[nm1] = 0. ;
+      for (int i=0; i<nr; i++)
+	cf0[i] = cf_tmp[i] ;
+      
+    } 	// fin de la boucle sur theta 
+  }	// fin de la boucle sur phi
+  
+  delete [] cf_tmp ;
+
 }
--- C++/Source/Non_class_members/Coef/cirleg.C
+++ C++/Source/Non_class_members/Coef/cirleg.C
@@ -33,6 +33,9 @@ char cirleg_C[] = "$Header$" ;
 /*
  * $Id$
  * $Log$
+ * Revision 1.2  2013/06/13 14:17:47  j_novak
+ * Implementation of Legendre inverse coefficient transform.
+ *
  * Revision 1.1  2013/06/06 15:31:33  j_novak
  * Functions to compute Legendre coefficients (not fully tested yet).
  *
@@ -142,17 +145,199 @@ void cirleg(const int* deg, const int* dimc, double* cf, const int* dimf,
     delete [] colloc ;
 }
 
-void cirlegp(const int* , const int* , double*, const int* ,
-		double* )
+//*****************************************************************************
+
+void cirlegp(const int* deg, const int* dimc, double* cf, 
+		    const int* dimf, double* ff)
+
 {
-  c_est_pas_fait("cirlegp") ;
+
+// Dimensions des tableaux ff et cf  :
+    int n1f = dimf[0] ;
+    int n2f = dimf[1] ;
+    int n3f = dimf[2] ;
+    int n1c = dimc[0] ;
+    int n2c = dimc[1] ;
+    int n3c = dimc[2] ;
+
+// Nombres de degres de liberte en r :    
+    int nr = deg[2] ;
+    
+// Tests de dimension:
+    if (nr > n3c) {
+	cout << "cirlegp: nr > n3c : nr = " << nr << " ,  n3c = " 
+	<< n3c << endl ;
+	abort () ;
+	exit(-1) ;
+    }
+    if (nr > n3f) {
+	cout << "cirlegp: nr > n3f : nr = " << nr << " ,  n3f = " 
+	<< n3f << endl ;
+	abort () ;
+	exit(-1) ;
+    }
+    if (n1c > n1f) {
+	cout << "cirlegp: n1c > n1f : n1c = " << n1c << " ,  n1f = " 
+	<< n1f << endl ;
+	abort () ;
+	exit(-1) ;
+    }
+    if (n2c > n2f) {
+	cout << "cirlegp: n2c > n2f : n2c = " << n2c << " ,  n2f = " 
+	<< n2f << endl ;
+	abort () ;
+	exit(-1) ;
+    }
+
+// Nombre de points
+    int nm1 = nr - 1;
+    int dnm1 = 2*nr - 1 ;
+
+// boucle sur phi et theta
+
+    int n2n3f = n2f * n3f ;
+    int n2n3c = n2c * n3c ;
+
+/*   
+ * Borne de la boucle sur phi: 
+ *    si n1c = 1, on effectue la boucle une fois seulement.
+ *    si n1c > 1, on va jusqu'a j = n1c-2 en sautant j = 1 (les coefficients
+ *	j=n1c-1 et j=0 ne sont pas consideres car nuls). 
+ */
+    int borne_phi = ( n1c > 1 ) ? n1c-1 : 1 ;
+
+    double* colloc = new double[dnm1] ;
+    legendre_collocation_points(dnm1, colloc) ;
+
+    for (int j=0; j< borne_phi; j++) {
+    
+	if (j==1) continue ;	// on ne traite pas le terme en sin(0 phi)
+
+	for (int k=0; k<n2c; k++) {
+
+	    int i0 = n2n3c * j + n3c * k ; // indice de depart 
+	    double* cf0 = cf + i0 ;    // tableau des donnees a transformer
+
+	    i0 = n2n3f * j + n3f * k ; // indice de depart 
+	    double* ff0 = ff + i0 ;    // tableau resultat
+
+	    for (int i = 0; i<nr; i++) {
+	      double x0 = colloc[nm1+i] ;
+	      double Pi = 1. ;
+	      double Pip1 = x0 ;
+	      double som = cf0[0] ;
+	      for (int h=2; h<dnm1; h++) {
+		double Pip2 = (2. - 1./double(h))*x0*Pip1
+		  - (1. - 1./double(h))*Pi ;
+		if (h%2 == 0) som += cf0[h/2]*Pip2 ;
+		Pi = Pip1 ;
+		Pip1 = Pip2 ;
+	      }
+	      ff0[i] = som ;
+	    }
+
+	} 	// fin de la boucle sur theta 
+   }	// fin de la boucle sur phi
+
+    delete [] colloc ;
 
 }
 
+//*****************************************************************************
 
-void cirlegi(const int* , const int* , double*, const int* ,
-		double* )
+void cirlegi(const int* deg, const int* dimc, double* cf, 
+		    const int* dimf, double* ff)
+
 {
-  c_est_pas_fait("cirlegi") ;
+
+// Dimensions des tableaux ff et cf  :
+    int n1f = dimf[0] ;
+    int n2f = dimf[1] ;
+    int n3f = dimf[2] ;
+    int n1c = dimc[0] ;
+    int n2c = dimc[1] ;
+    int n3c = dimc[2] ;
+
+// Nombres de degres de liberte en r :    
+    int nr = deg[2] ;
+    
+// Tests de dimension:
+    if (nr > n3c) {
+	cout << "cirlegi: nr > n3c : nr = " << nr << " ,  n3c = " 
+	<< n3c << endl ;
+	abort () ;
+	exit(-1) ;
+    }
+    if (nr > n3f) {
+	cout << "cirlegi: nr > n3f : nr = " << nr << " ,  n3f = " 
+	<< n3f << endl ;
+	abort () ;
+	exit(-1) ;
+    }
+    if (n1c > n1f) {
+	cout << "cirlegi: n1c > n1f : n1c = " << n1c << " ,  n1f = " 
+	<< n1f << endl ;
+	abort () ;
+	exit(-1) ;
+    }
+    if (n2c > n2f) {
+	cout << "cirlegi: n2c > n2f : n2c = " << n2c << " ,  n2f = " 
+	<< n2f << endl ;
+	abort () ;
+	exit(-1) ;
+    }
+
+// Nombre de points
+    int nm1 = nr - 1;
+    int dnm1 = 2*nr - 1 ;
+
+// boucle sur phi et theta
+
+    int n2n3f = n2f * n3f ;
+    int n2n3c = n2c * n3c ;
+
+/*   
+ * Borne de la boucle sur phi: 
+ *    si n1c = 1, on effectue la boucle une fois seulement.
+ *    si n1c > 1, on va jusqu'a j = n1c-2 en sautant j = 1 (les coefficients
+ *	j=n1c-1 et j=0 ne sont pas consideres car nuls). 
+ */
+    int borne_phi = ( n1c > 1 ) ? n1c-1 : 1 ;
+
+    double* colloc = new double[dnm1] ;
+    legendre_collocation_points(dnm1, colloc) ;
+
+    for (int j=0; j< borne_phi; j++) {
+    
+	if (j==1) continue ;	// on ne traite pas le terme en sin(0 phi)
+
+	for (int k=0; k<n2c; k++) {
+
+	    int i0 = n2n3c * j + n3c * k ; // indice de depart 
+	    double* cf0 = cf + i0 ;    // tableau des donnees a transformer
+
+	    i0 = n2n3f * j + n3f * k ; // indice de depart 
+	    double* ff0 = ff + i0 ;    // tableau resultat
+
+	    for (int i = 0; i<nr; i++) {
+	      double x0 = colloc[nm1+i] ;
+	      double Pi = 1. ;
+	      double Pip1 = x0 ;
+	      double som = cf0[0]*x0 ;
+	      for (int h=2; h<dnm1; h++) {
+		double Pip2 = (2. - 1./double(h))*x0*Pip1
+		  - (1. - 1./double(h))*Pi ;
+		if (h%2 == 1) som += cf0[h/2]*Pip2 ;
+		Pi = Pip1 ;
+		Pip1 = Pip2 ;
+	      }
+	      ff0[i] = som ;
+	    }
+
+	} 	// fin de la boucle sur theta 
+   }	// fin de la boucle sur phi
+
+    delete [] colloc ;
 
 }
+
