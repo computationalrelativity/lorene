0ce57b96606dd48ab70061f273da66362e19053f
f_vincent <f_vincent>
2015-10-22 09:20:02 +0000

    New code scalarbh

---
+++ Codes/Alternatives/par_scalarbh.d
@@ -0,0 +1,9 @@
+BSCarlosTest.dat
+1.  mass M
+0   graphical outputs: 0 = no, 1 = yes
+33	nr: number of points in r (the same in each domain)
+7	nt: number of points in theta (the same in each domain)
+1	np: number of points in phi (the same in each domain)
+6	nz: number of domains
+# inner value of r in each domain (in units of M) :
+0. 0.5 1. 2. 4. 8.  
---
+++ Codes/Alternatives/scalarbh.C
@@ -0,0 +1,171 @@
+/*
+ *  Code for reading scalar-hair BH from a file
+ *  Scalar-hair BH are the KBHsSH described in Herdeiro & Radu (2015)
+ *  CQG, 32, 144001 
+ */
+
+/*
+ *   Copyright (c) 2015 Frederic Vincent, Eric Gourgoulhon
+ *
+ *   This file is part of LORENE.
+ *
+ *   LORENE is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License version 2
+ *   as published by the Free Software Foundation.
+ *
+ *   LORENE is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with LORENE; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+char scalarbh_C[] = "$Header$" ;
+
+/*
+ * $Id$
+ * $Log$
+ * Revision 1.1  2015/10/22 09:20:02  f_vincent
+ * New code scalarbh
+ *
+ *
+ * $Header$
+ *
+ */
+
+// C++ headers
+#include "headcpp.h"
+
+// C headers
+#include <cstdlib>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+using namespace std ;
+
+// Lorene headers
+#include "compobj.h"
+#include "nbr_spx.h"
+#include "utilitaires.h"
+#include "proto.h"
+#include "graphique.h"
+
+using namespace Lorene ;
+
+int main() {
+
+  // Parameters of the computation
+  // -----------------------------
+    
+  ifstream fpar("par_scalarbh.d") ;
+  if ( !fpar.good() ) {
+    cerr << "Problem in opening the file par_scalarbh.d ! " << endl ;
+    abort() ;
+  }
+    
+  char file_name[256] ; 
+  fpar.getline(file_name, 256) ;
+  cout << "File to be read: " << file_name << endl ; 
+
+  double mass ; // M
+  fpar >> mass ; fpar.ignore(1000,'\n') ;
+    
+  int graphic_out ; // flag for graphical outputs
+  fpar >> graphic_out ; fpar.ignore(1000,'\n') ; 
+
+  int nr ; // Number of collocation points in r in each domain
+  fpar >> nr; fpar.ignore(1000,'\n') ;
+
+  int nt ; // Number of collocation points in theta in each domain
+  fpar >> nt; fpar.ignore(1000,'\n') ;
+
+  int np ; // Number of collocation points in phi in each domain
+  fpar >> np; fpar.ignore(1000,'\n') ;
+
+  int nz ; // Number of domains
+  fpar >> nz ; fpar.ignore(1000,'\n') ;
+  int nzm1 = nz - 1 ; // Index of outermost domain
+
+  fpar.ignore(1000,'\n') ; // skip title
+  double* r_limits = new double[nz+1];  // inner boundaries of each domain in units of M      
+  for (int l=0; l<nz; l++) 
+    {
+      fpar >> r_limits[l]; 
+    }
+  r_limits[nz] = __infinity ;
+    
+  fpar.close();
+
+
+  // Setup of a multi-domain grid (Lorene class Mg3d)
+  // ------------------------------------------------
+  
+  int symmetry_theta = SYM ; // symmetry with respect to the equatorial plane
+  int symmetry_phi = SYM ; // symmetry with respect to phi --> phi + pi
+  bool compact = true ; // external domain is compactified
+
+  Mg3d mgrid(nz, nr, nt, np, symmetry_theta, symmetry_phi, compact) ;
+
+  //cout << mgrid << endl ; 
+
+  
+  // Setup of an affine mapping : grid --> physical space (Lorene class Map_af)
+  // --------------------------------------------------------------------------
+  
+  Map_af map(mgrid, r_limits) ;
+
+  // Construction of the ScalarBH object:
+  // ----------------------------------
+
+  ScalarBH bh(map, file_name) ; 
+
+  //bh.update_metric() ; // TO BE ADDED WHEN WRITTEN
+
+  cout.precision(15) ; 
+  cout << endl << "******* bh ******** " << endl ;  
+  cout << bh << endl ; 
+    
+  // Drawings    
+  if (graphic_out == 1) 
+    {
+      double r_max = 1.5*map.val_r(nzm1,-1.,0.,0.) ; 
+      des_meridian(bh.get_nn(), 0, r_max, "N", 1) ; 
+      //des_meridian(diffn , 0, r_max, "N - N(2)", 2) ; 
+      //des_meridian(diffddn , 0, r_max, "diff d2N", 3) ; 
+	
+      //des_meridian(bh.get_nphi(), 0, r_max, "Nphi", 4) ; 
+      //des_meridian(bh.get_nphi().dsdr(), 0, r_max, "dNphi/dr", 5) ; 
+	
+      des_meridian(bh.get_gamma().cov()(1,1), 0, r_max, "gamma_11", 6) ; 
+      des_meridian(bh.get_gamma().cov()(3,3), 0, r_max, "gamma_33", 7) ; 
+	
+      des_meridian(bh.get_kk()(1,3), 0.4, 0.6, "K_(r)(ph)", 8) ; 
+      //des_meridian(bh.get_krphi(), 0.4, 0.6, "K_(r)(ph)/sin(theta) from file", 9) ; 
+      des_meridian(bh.get_kk()(2,3), 0, r_max, "K_(th)(ph)", 10) ; 
+      //arrete() ; 
+    }
+    
+  //----------------------
+  // Output file for GYOTO
+  //----------------------
+  bh.gyoto_data("gyoto_scalarBH.d") ;    
+    
+  return EXIT_SUCCESS ; 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
